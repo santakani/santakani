c91195e8f78521c8b4c17188108d88c2d3c6cadc
Guo Yunhe <guoyunhebrave@gmail.com>
2016-03-13 12:16:33 +0200

    Design media table scheme.

--- database/migrations/2016_03_13_092526_create_media_table.php
+++ database/migrations/2016_03_13_092526_create_media_table.php
@@ -14,7 +14,18 @@ class CreateMediaTable extends Migration
     {
         Schema::create('media', function (Blueprint $table) {
             $table->increments('id');
+
+            $table->string('type'); // Image, audio, video
+            $table->string('format'); // jpg, png, gif
+            $table->integer('width')->unsigned(); // Width of image and video
+            $table->integer('height')->unsigned(); // Height of image and video
+            $table->string('external_url'); // YouTube, Vimeo, SoundCloud, etc.
+            // Large size image: /upload/[id]/large.[format] max. 1200x1200px
+            // Small size image: /upload/[id]/small.[format] max. 600x600px
+
+            // Timestamps
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
