c320aaabb7236667bb246f316fb467fb360c00a6
Guo Yunhe <guoyunhebrave@gmail.com>
2016-03-16 18:08:31 +0200

    Add user_id foreign key.

---
+++ database/migrations/2016_03_16_155644_add_user_id_foreign_key.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserIdForeignKey extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('place', function (Blueprint $table) {
+            // When deleted image, set image_id to null
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+        });
+        Schema::table('image', function (Blueprint $table) {
+            // When deleted image, set image_id to null
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+        });
+        Schema::table('article', function (Blueprint $table) {
+            // When deleted image, set image_id to null
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('place', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
+        Schema::table('image', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
+        Schema::table('article', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
+    }
+}
