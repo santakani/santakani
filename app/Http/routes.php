b267c42eb69cde520572eb0941f4d8498025caed
Guo Yunhe <guoyunhebrave@gmail.com>
2016-03-17 10:12:21 +0200

    Basic auth routes work now.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 /*
@@ -26,12 +26,8 @@ Route::get('/', function () {
 |
 */
 
-Route::group(['middleware' => ['web']], function () {
-    //
-});
-
 Route::group(['middleware' => 'web'], function () {
     Route::auth();
 
-    Route::get('/home', 'HomeController@index');
+    Route::get('/', 'HomeController@index');
 });
