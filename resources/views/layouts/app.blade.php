50304a1925389673658d6a8d2a5141a19a369eb8
Guo Yunhe <guoyunhebrave@gmail.com>
2016-03-18 09:11:42 +0200

    Change title.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>{{ isset($title) ? ($title.' - Santakani') : 'Santakani - Discover independent design from Finland' }}</title>
 
     <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
     <link href="bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
