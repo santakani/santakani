4f8bf61f1061a1f875beb9b304813308608815eb
Guo Yunhe <guoyunhebrave@gmail.com>
2016-04-03 13:38:35 +0300

    User table seeding.

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -11,6 +11,20 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('user')->insert([
+            'name' => 'Guo Yunhe',
+            'email' => 'yunhe.guo@santakani.com',
+            'password' => bcrypt('abcd123456'),
+        ]);
+        DB::table('user')->insert([
+            'name' => 'Du Yuexin',
+            'email' => 'yuexin.du@santakani.com',
+            'password' => bcrypt('abcd123456'),
+        ]);
+        DB::table('user')->insert([
+            'name' => 'Yun Xiaotong',
+            'email' => 'xiaotong.yun@santakani.com',
+            'password' => bcrypt('abcd123456'),
+        ]);
     }
 }
