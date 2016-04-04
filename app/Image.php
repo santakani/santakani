ab2b938bd4350fb1b4afc8aa2a1c59b76d854718
Guo Yunhe <guoyunhebrave@gmail.com>
2016-04-04 18:13:16 +0300

    Assign table to model class.

--- app/Image.php
+++ app/Image.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'image';
 }
