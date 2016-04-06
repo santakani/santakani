0433b54b0b75a69d8ca38f7dc17a5ab75aa7ed1b
Guo Yunhe <guoyunhebrave@gmail.com>
2016-04-06 08:58:29 +0300

    Add designer translation model.

---
+++ app/DesignerTranslation.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DesignerTranslation extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'designer_translation';
+}
