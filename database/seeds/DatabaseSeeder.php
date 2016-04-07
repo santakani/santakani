010647ddcc4e62efc420fcfb6ff050543b1ac0bd
Guo Yunhe <guoyunhebrave@gmail.com>
2016-04-07 10:27:21 +0300

    Remove seeds of non-existing tables.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,16 +13,14 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UserTableSeeder::class);
         $this->call(ImageTableSeeder::class);
+        $this->call(TagTableSeeder::class);
         $this->call(CountryTableSeeder::class);
         $this->call(CityTableSeeder::class);
         $this->call(PlaceTableSeeder::class);
-        $this->call(DesignerTableSeeder::class);
-        $this->call(TagTableSeeder::class);
         $this->call(PlaceTagTableSeeder::class);
-        $this->call(DesignerTagTableSeeder::class);
-        $this->call(CountryImageTableSeeder::class);
-        $this->call(CityImageTableSeeder::class);
         $this->call(PlaceImageTableSeeder::class);
+        $this->call(DesignerTableSeeder::class);
+        $this->call(DesignerTagTableSeeder::class);
         $this->call(DesignerImageTableSeeder::class);
     }
 }
