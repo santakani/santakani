72a582e59efba702eafe5dfac3f23fb8a3b047f6
Guo Yunhe <guoyunhebrave@gmail.com>
2016-04-07 11:25:31 +0300

    Seed up country table.

--- database/seeds/CountryTableSeeder.php
+++ database/seeds/CountryTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class CountryTableSeeder extends Seeder
 {
@@ -11,6 +12,28 @@ class CountryTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // 1
+        DB::table('country')->insert([
+            'url_name' => 'finland',
+            'image_id' => 11,
+            'country_code' => 'FI',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('country_translation')->insert([
+            'country_id' => 1,
+            'language' => 'en',
+            'name' => 'Finland',
+            'content' => file_get_contents('http://loripsum.net/api'),
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('country_translation')->insert([
+            'country_id' => 1,
+            'language' => 'zh',
+            'name' => '芬兰',
+            'content' => file_get_contents('http://loripsum.net/api'),
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
     }
 }
