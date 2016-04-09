0f1833ada73b2c6dc626b10006208ffd90a9fb7f
Guo Yunhe <guoyunhebrave@gmail.com>
2016-04-09 16:11:21 +0300

    Fix variable name.

--- resources/views/places.blade.php
+++ resources/views/places.blade.php
@@ -9,10 +9,10 @@
         <div class="grid row">
             @foreach ($places as $place)
                 <article id="place-{{ $place->id }}" class="place grid-item col-xs-12 col-sm-6 col-md-4 col-lg-3">
-                    <img class="featured-image" src="{{ $designer->getImage()->getThumbUrl() }}" />
-                    <h3 class="title"><a href="{{ url('/designer/' . $designer->id) }}">
-                        {{ $designer->getTranslation()->name }}</a></h3>
-                    <div class="content">{!! $designer->getTranslation()->content !!}</div>
+                    <img class="featured-image" src="{{ $place->getImage()->getThumbUrl() }}" />
+                    <h3 class="title"><a href="{{ url('/place/' . $place->id) }}">
+                        {{ $place->getTranslation()->name }}</a></h3>
+                    <div class="content">{!! $place->getTranslation()->content !!}</div>
                     <div class="expand-button btn btn-sm btn-default">
                         <span class="more"><i class="fa fa-angle-down"></i> More</span>
                         <span class="less"><i class="fa fa-angle-up"></i> Less</span>
