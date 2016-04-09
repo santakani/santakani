1e7e32dfde05a99bff472ccec2f459688d19ccd3
Guo Yunhe <guoyunhebrave@gmail.com>
2016-04-09 20:52:02 +0300

    Config email sender.

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'noreply@santakani.com', 'name' => 'SantaKani'],
 
     /*
     |--------------------------------------------------------------------------
