00e15ef1f3966b74aa4ce115afdc702ce6f3001d
Guo Yunhe <guoyunhebrave@gmail.com>
2016-04-09 18:27:39 +0300

    Design place list view.

--- resources/views/places.blade.php
+++ resources/views/places.blade.php
@@ -6,21 +6,33 @@
         <div id="place-map-inner"></div>
     </div>
     <div id="place-list">
-        <div class="grid row">
-            @foreach ($places as $place)
-                <article id="place-{{ $place->id }}" class="place grid-item col-xs-12 col-sm-6 col-md-4 col-lg-3">
-                    <img class="featured-image" src="{{ $place->getImage()->getThumbUrl() }}" />
-                    <h3 class="title"><a href="{{ url('/place/' . $place->id) }}">
-                        {{ $place->getTranslation()->name }}</a></h3>
-                    <div class="content">{!! $place->getTranslation()->content !!}</div>
-                    <div class="expand-button btn btn-sm btn-default">
-                        <span class="more"><i class="fa fa-angle-down"></i> More</span>
-                        <span class="less"><i class="fa fa-angle-up"></i> Less</span>
+        @foreach ($places as $place)
+            <article id="place-{{ $place->id }}" class="place">
+                <div class="image" style="background-image:url({{ $place->getImage()->getThumbUrl() }})">
+                    <span class="type">{{ $place->type }}</span>
+                </div>
+                <div class="text">
+                    <h3 class="title">
+                        <a href="{{ url('/place/' . $place->id) }}">
+                            {{ $place->getTranslation()->name }}
+                        </a>
+                        <small>
+                            <i class="fa fa-heart-o"></i> 59
+                            <i class="fa fa-comment-o"></i> 14
+                        </small>
+                    </h3>
+                    <p class="tags">
+                        <span class="tag">grocery</span>
+                        <span class="tag">tableware</span>
+                        <span class="tag">knitwear</span>
+                    </p>
+                    <div class="contnet">
+                        {!! $place->getTranslation()->content !!}
                     </div>
-                </article>
-            @endforeach
-        </div><!-- .grid.row -->
-    </div><!-- #place-list.container -->
+                </div>
+            </article>
+        @endforeach
+    </div><!-- #place-list -->
 </div>
 <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_API_KEY') }}&callback=initMap"></script>
 @endsection
