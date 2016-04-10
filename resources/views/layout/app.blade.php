622fa4475882f1e519dccaf9faf5a865dae0bbe0
Guo Yunhe <guoyunhebrave@gmail.com>
2016-04-10 13:26:49 +0300

    Kanibar responsive design.

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -42,35 +42,33 @@
                 </a>
             </div>
 
-            <div id="user-menu">
-                @if (Auth::guest())
-                    <a href="{{ url('/login') }}">Login</a> /
+            @if (Auth::guest())
+                <div id="user-menu" class="not-logged-in">
+                    <a href="{{ url('/login') }}">Login</a>
                     <a href="{{ url('/register') }}">Register</a>
-                @else
-                    <div class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                            <span class="avatar" style="background-image:url({{Auth::user()->getAvatarUrl()}});"></span>
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a href="{{ url('/dashboard') }}">
-                                <i class="fa fa-fw fa-tachometer"></i> Dashboard</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li><a href="{{ url('/user/'.Auth::user()->id) }}">
-                                <i class="fa fa-fw fa-user"></i> Profile</a></li>
-                            <li><a href="{{ url('/notification') }}">
-                                <i class="fa fa-fw fa-bell"></i> Notification</a></li>
-                            <li><a href="{{ url('/setting') }}">
-                                <i class="fa fa-fw fa-cog"></i> Settings</a></li>
-                            <li><a href="{{ url('/logout') }}">
-                                <i class="fa fa-fw fa-sign-out"></i> Logout</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li><a href="#" data-toggle="modal" data-target="#language-modal">
-                                <i class="fa fa-fw fa-globe"></i> English - EUR - Finland</a></li>
-                        </ul>
-                    </div>
-                @endif
-            </div>
+                </div><!-- #user-menu.not-logged-in -->
+            @else
+                <div id="user-menu" class="dropdown logged-in">
+                    <a href="#" class="avatar dropdown-toggle" style="background-image:url({{Auth::user()->getAvatarUrl()}});" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-right">
+                        <li><a href="{{ url('/dashboard') }}">
+                            <i class="fa fa-fw fa-tachometer"></i> Dashboard</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="{{ url('/user/'.Auth::user()->id) }}">
+                            <i class="fa fa-fw fa-user"></i> Profile</a></li>
+                        <li><a href="{{ url('/notification') }}">
+                            <i class="fa fa-fw fa-bell"></i> Notification</a></li>
+                        <li><a href="{{ url('/setting') }}">
+                            <i class="fa fa-fw fa-cog"></i> Settings</a></li>
+                        <li><a href="{{ url('/logout') }}">
+                            <i class="fa fa-fw fa-sign-out"></i> Logout</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#" data-toggle="modal" data-target="#language-modal">
+                            <i class="fa fa-fw fa-globe"></i> English - EUR - Finland</a></li>
+                    </ul>
+                </div>
+            @endif
         </div><!-- .first-row -->
         <div class="second-row">
             <form role="search" action="{{ url('/search') }}">
