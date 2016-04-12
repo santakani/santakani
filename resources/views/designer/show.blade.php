1b2abb50254a4449db1ec56059e1226c7a0ac277
Guo Yunhe <guoyunhebrave@gmail.com>
2016-04-12 13:52:56 +0300

    Design designer page. Add text shadow to kanibar text.

--- resources/views/designer/show.blade.php
+++ resources/views/designer/show.blade.php
@@ -12,13 +12,7 @@
 
 
 <article id="designer-{{ $designer->id }}" class="designer container">
-    <img class="featured-image" src="{{ $designer->getImage()->getThumbUrl() }}" />
-    <h3 class="title"><a href="{{ url('/designer/' . $designer->id) }}">
-        {{ $designer->getTranslation()->name }}</a></h3>
+    <h1 class="title">{{ $designer->getTranslation()->name }}</h1>
     <div class="content">{!! $designer->getTranslation()->content !!}</div>
-    <div class="expand-button btn btn-sm btn-default">
-        <span class="more"><i class="fa fa-angle-down"></i> More</span>
-        <span class="less"><i class="fa fa-angle-up"></i> Less</span>
-    </div>
 </article>
 @endsection
