5df3a39dcc0eaa46a37ab25092a933c7fa91f165
Guo Yunhe <guoyunhebrave@gmail.com>
2016-04-17 15:12:46 +0300

    Designer edit page layout and interaction.

--- resources/views/designer/edit.blade.php
+++ resources/views/designer/edit.blade.php
@@ -5,6 +5,66 @@
     'back_link' => url('/designer/' . $designer->id),
 ])
 
-@section('content')
-<button class="btn btn-default" type="submit">Save</button>
+@section('edit_content')
+<form class="form-horizontal" action="{{ url('designer/'.$designer->id) }}"
+    method="put">
+
+    <div class="form-group">
+        <label for="input-name" class="col-sm-2 control-label">
+            Name</label>
+        <div class="col-sm-10">
+            <input type="text" class="form-control" id="input-name"
+                placeholder="Name of designer or design brand">
+        </div>
+    </div>
+
+    <div class="form-group">
+        <label for="input-content" class="col-sm-2 control-label">
+            Story</label>
+        <div class="col-sm-10">
+            <textarea class="form-control" id="input-content" rows="3"
+                placeholder="Introduce your unique design story..."></textarea>
+        </div>
+    </div>
+
+    <div class="form-group">
+        <label class="col-sm-2 control-label">
+            Main image</label>
+        <div class="col-sm-10">
+            <p>
+                <button type="button" id="button-main-image" class="btn btn-default">Change</button>
+                <input type="file" id="input-main-image" class="hidden" accept="image/*">
+            </p>
+            <div id="main-image-preview" class="image-preview"
+                style="background-image:url({{$designer->getMainImage()->getThumbUrl()}})"
+                data-image-id="{{$designer->getMainImage()->id}}"></div>
+        </div>
+    </div>
+
+    <div class="form-group">
+        <label class="col-sm-2 control-label">
+            Image gallery</label>
+        <div class="col-sm-10">
+            <p>
+                <button type="button" id="button-upload-image" class="btn btn-default">Upload</button>
+                <input type="file" id="input-upload-image" class="hidden" accept="image/*">
+            </p>
+            <div id="image-gallery">
+            @foreach ($designer->getImages() as $image)
+                <div class="image-preview" data-image-id="{{$designer->getMainImage()->id}}"
+                    style="background-image:url({{$image->getThumbUrl()}})">
+                    <span><i class="fa fa-times"></i></span>
+                </div>
+            @endforeach
+            </div>
+        </div>
+    </div>
+
+    <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="btn btn-default">Save</button>
+        </div>
+    </div>
+
+</form>
 @endsection
