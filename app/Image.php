06cc37fae83912f50779b51f5bb565cd93ac812d
Guo Yunhe <guoyunhebrave@gmail.com>
2016-04-18 20:15:29 +0300

    Function to save uploaded image.

--- app/Image.php
+++ app/Image.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\HttpFoundation\File\File;
 
 class Image extends Model
 {
@@ -90,11 +91,77 @@ class Image extends Model
             $size = 'small';
         }
 
-        if ($size === 'thumb' && $this->width <= self::small_thumb_size && $this->width <= self::small_thumb_size) {
+        if ($size === 'thumb' && ($this->width <= self::small_thumb_size || $this->width <= self::small_thumb_size)) {
             $size = 'small-thumb';
         }
 
         return $size;
+    }
+
+    /**
+     * Detect if a size exists
+     *
+     * @param string $size
+     *
+     * @return boolean
+     */
+    public function hasSize($size)
+    {
+        if ($size === 'large' && $this->width <= self::medium_size && $this->width <= self::medium_size) {
+            return false;
+        }
+
+        if ($size === 'medium' && $this->width <= self::small_size && $this->width <= self::small_size) {
+            return false;
+        }
+
+        if ($size === 'thumb' && ($this->width <= self::small_thumb_size || $this->width <= self::small_thumb_size)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Generate full path to image directory
+     *
+     * If image id is 1009768, it will be stored in public/storage/image/1009/768
+     *
+     * @return string
+     */
+    public function getDirPath()
+    {
+        $path = 'storage/image/' . (int)($this->id/1000) . '/' . $this->id%1000;
+
+        return public_path($path);
+    }
+
+    /**
+     * Generate full path to image file
+     *
+     * If image id is 1009768, it will be stored in public/storage/image/1009/768 and contains:
+     * - large.jpg
+     * - medium.jpg
+     * - small.jpg
+     * - thumb.jpg
+     * - thumb-small.jpg
+     *
+     * Even if the requested size doesn't exist, still return the expected path.
+     *
+     * @param string $size One of large, medium, small, thumb, thumb-small
+     *
+     * @return string
+     */
+    public function getFilePath($size = 'large')
+    {
+        if (!empty($this->external_url)) {
+            return null;
+        }
+
+        $path = 'storage/image/' . (int)($this->id/1000) . '/' . $this->id%1000
+                . '/' . $size . $this->getFileExtension();
+
+        return public_path($path);
     }
 
     /**
@@ -106,6 +173,9 @@ class Image extends Model
      * - small.jpg
      * - thumb.jpg
      * - thumb-small.jpg
+     *
+     * If the requested size doesn't exist, return a fallback size (smaller).
+     *
      * @param string $size One of large, medium, small, thumb, thumb-small
      *
      * @return string
@@ -140,6 +210,90 @@ class Image extends Model
     public function getSmallThumbUrl()
     {
         return $this->getUrl('thumb-small');
+    }
+
+    /**
+     * Save image file and generate sizes.
+     *
+     * @param Symfony\Component\HttpFoundation\File\File $file
+     */
+    public function saveFile(File $file)
+    {
+        $new_file = $file->move($this->getDirPath(), 'temp');
+        $temp_file_path = $this->getDirPath() . 'temp';
+
+        $image = new Imagick($temp_file_path);
+
+        // Read original size. Before this, width and height properties are empty.
+        $this->width = $image->getImageWidth();
+        $this->height = $image->getImageHeight();
+
+        $new_width = 0;
+        $new_height = 0;
+
+        if ($this->hasSize('large')) {
+            $width = min(self::large_size, $this->width);
+            $height = min(self::large_size, $this->height);
+
+            $image->thumbnailImage($width, $height, true);
+            $image->writeImage($this->getFilePath('large'));
+
+            if ($new_width !== 0) {
+                $new_width = $image->getImageWidth();
+                $new_height = $image->getImageHeight();
+            }
+        }
+
+        if ($this->hasSize('medium')) {
+            $width = min(self::medium_size, $this->width);
+            $height = min(self::medium_size, $this->height);
+
+            $image->thumbnailImage($width, $height, true);
+            $image->writeImage($this->getFilePath('medium'));
+
+            if ($new_width !== 0) {
+                $new_width = $image->getImageWidth();
+                $new_height = $image->getImageHeight();
+            }
+        }
+
+        if ($this->hasSize('small')) {
+            $width = min(self::small_size, $this->width);
+            $height = min(self::small_size, $this->height);
+
+            $image->thumbnailImage($width, $height, true);
+            $image->writeImage($this->getFilePath('small'));
+
+            if ($new_width !== 0) {
+                $new_width = $image->getImageWidth();
+                $new_height = $image->getImageHeight();
+            }
+        }
+
+        $image->readImage($temp_file_path);
+
+        if ($this->hasSize('thumb')) {
+            $size = min(self::thumb_size, $this->width, $this->height);
+
+            $image->cropThumbnailImage($size, $size);
+            $image->writeImage($this->getFilePath('thumb'));
+        }
+
+        if ($this->hasSize('thumb-small')) {
+            $size = min(self::small_thumb_size, $this->width, $this->height);
+
+            $image->cropThumbnailImage($size, $size);
+            $image->writeImage($this->getFilePath('thumb-small'));
+        }
+
+        $image->destroy();
+
+        unlink($temp_file_path);
+
+        // Update resized width and height
+        $this->width = $new_width;
+        $this->height = $new_height;
+        $this->save();
     }
 
 
