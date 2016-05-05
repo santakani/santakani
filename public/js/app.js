1f95319681c4f398eae09db3b61deeff9b5a9f33
Guo Yunhe <guoyunhebrave@gmail.com>
2016-05-05 08:50:43 +0300

    Improve sass and js folder structure.

--- public/js/app.js
+++ public/js/app.js
@@ -1,61 +1,3 @@
-$(function () {
-    $('#picture-carousel').flickity({
-        cellAlign: 'left',
-        freeScroll: true
-    });
-
-    $('#picture-carousel').lightGallery({
-        selector: '.picture-thumb',
-        exThumbImage: 'data-thumb',
-        thumbWidth: 100,
-        thumbContHeight: 120
-    });
-});
-
- 
-
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('place-map-draw'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
-}
-
-$(function () {
-    $('#place-map-inner').affix({
-        offset: {
-            top: function () {
-                return this.top = $('#kanibar').outerHeight(true) + 20;
-            },
-            bottom: function () {
-                return this.bottom = $('footer').outerHeight(true) + 20;
-            }
-        }
-    });
-});
-
-$(function () {
-
-    var $grid = $('#story-list .grid')
-
-    if ($grid.length === 0) {
-        return;
-    }
-
-    $grid.masonry({
-        itemSelector: '.grid-item', // use a separate class for itemSelector, other than .col-
-        columnWidth: '.grid-item',
-        percentPosition: true
-    });
-
-    $grid.find('.story .expand-button').click(function () {
-        $(this).parent('.story').toggleClass('expanded');
-        $(this).siblings('.content').scrollTop(0);
-        $grid.masonry();
-    });
-});
-
 // A custom jQuery plugin to scroll window to specific element.
 // Useage: $('#my-div').goTo();
 
@@ -67,123 +9,6 @@ $(function () {
         return this; // for chaining...
     }
 })(jQuery);
-
-/**
- * Control designer edit form.
- *
- * View - views/designer/edit.blade.php
- * Style - assets/sass/_edit-layout.scss
- * Script - assets/js/edit/ajax/designer-edit.js
- */
-
-$(function () {
-
-    // Page check
-    if ($('#designer-edit-page').length === 0) {
-        return;
-    }
-
-    // Initialize TinyMCE
-    tinymce.init({
-        selector: 'textarea.tinymce',
-        menubar: false,
-        content_css: ['/css/app.css', '/css/editor.css'],
-        setup: function (editor) {
-            editor.on('change', function () {
-                editor.save();
-            });
-        }
-    });
-
-    // Initialize ImageUploader for main image
-    var imagePreview = new ImagePreview({
-        selector: '#image-form-group .image-preview'
-    });
-
-    var imageUploader = new ImageUploader({
-        selector: '#image-form-group .image-uploader',
-        start: function () {
-            imagePreview.progress('show');
-        },
-        progress: function (percentage) {
-            imagePreview.progress(percentage);
-        },
-        done: function (image) {
-            imagePreview.progress('hide');
-            imagePreview.image(image.id, image.url.thumb);
-        },
-        fail: function (error) {
-            imagePreview.progress('hide');
-            console.log(error);
-        }
-    });
-
-    // Initialize ImageUploader for image gallery
-    $('#gallery-form-group .image-gallery .image-preview').each(function () {
-        var preview = new ImagePreview({
-            element: this
-        });
-    });
-
-    Sortable.create($('.image-gallery')[0], {animation: 300});
-
-    var newPreviews = [];
-
-    var galleryUploader = new ImageUploader({
-        selector: '#gallery-form-group .image-uploader',
-        multiple: true,
-        start: function (index) {
-            var preview = new ImagePreview({
-                template: $($('#gallery-form-group template').prop('content')).find('.image-preview')
-            });
-            $('.image-gallery').append(preview.element);
-            preview.progress('show');
-            newPreviews.push(preview);
-        },
-        progress: function (percentage, index) {
-            newPreviews[index].progress(percentage);
-        },
-        done: function (image, index) {
-            newPreviews[index].progress('hide');
-            newPreviews[index].image(image.id, image.url.thumb);
-            console.log(index);
-        },
-        fail: function (error, index) {
-            newPreviews[index].progress('hide');
-            console.log(error);
-        }
-    });
-
-    // Submit form
-    $('button[type="submit"]').click(function (e) {
-        e.preventDefault();
-
-        $.ajax({
-            method: 'PUT',
-            url: $('#designer-edit-form').attr('action'),
-            data: $('#designer-edit-form').serializeArray()
-        }).done(function () {
-            window.location.href = $('#designer-edit-form').attr('action');
-        }).fail(function (error) {
-            var response = error.responseJSON;
-            var $alert = $('#designer-edit-form .alert');
-            var message = '';
-
-            for (var id in response) {
-                message += '<p>' + response[id] + '</p>';
-            }
-
-            $alert.html(message).show().goTo();
-        });
-    });
-
-    // Country and city select, use Select2.
-    $("select#country-select").select2();
-    $("select#city-select").select2();
-
-    // Tag select, use Select2
-    $("select#tag-select").select2({tags: true});
-});
 
 var ImagePreview = function (options) {
     this.init(options).bindEvents();
@@ -409,5 +234,178 @@ ImageUploader.prototype.bindEvents = function () {
         uploader.chooseModal.modal('show');
     });
 };
+
+/**
+ * Control designer edit form.
+ *
+ * View - views/designer/edit.blade.php
+ * Style - assets/sass/_edit-layout.scss
+ * Script - assets/js/edit/ajax/designer-edit.js
+ */
+
+$(function () {
+
+    // Page check
+    if ($('#designer-edit-page').length === 0) {
+        return;
+    }
+
+    // Initialize TinyMCE
+    tinymce.init({
+        selector: 'textarea.tinymce',
+        menubar: false,
+        content_css: ['/css/app.css', '/css/editor.css'],
+        setup: function (editor) {
+            editor.on('change', function () {
+                editor.save();
+            });
+        }
+    });
+
+    // Initialize ImageUploader for main image
+    var imagePreview = new ImagePreview({
+        selector: '#image-form-group .image-preview'
+    });
+
+    var imageUploader = new ImageUploader({
+        selector: '#image-form-group .image-uploader',
+        start: function () {
+            imagePreview.progress('show');
+        },
+        progress: function (percentage) {
+            imagePreview.progress(percentage);
+        },
+        done: function (image) {
+            imagePreview.progress('hide');
+            imagePreview.image(image.id, image.url.thumb);
+        },
+        fail: function (error) {
+            imagePreview.progress('hide');
+            console.log(error);
+        }
+    });
+
+    // Initialize ImageUploader for image gallery
+    $('#gallery-form-group .image-gallery .image-preview').each(function () {
+        var preview = new ImagePreview({
+            element: this
+        });
+    });
+
+    Sortable.create($('.image-gallery')[0], {animation: 300});
+
+    var newPreviews = [];
+
+    var galleryUploader = new ImageUploader({
+        selector: '#gallery-form-group .image-uploader',
+        multiple: true,
+        start: function (index) {
+            var preview = new ImagePreview({
+                template: $($('#gallery-form-group template').prop('content')).find('.image-preview')
+            });
+            $('.image-gallery').append(preview.element);
+            preview.progress('show');
+            newPreviews.push(preview);
+        },
+        progress: function (percentage, index) {
+            newPreviews[index].progress(percentage);
+        },
+        done: function (image, index) {
+            newPreviews[index].progress('hide');
+            newPreviews[index].image(image.id, image.url.thumb);
+            console.log(index);
+        },
+        fail: function (error, index) {
+            newPreviews[index].progress('hide');
+            console.log(error);
+        }
+    });
+
+    // Submit form
+    $('button[type="submit"]').click(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            method: 'PUT',
+            url: $('#designer-edit-form').attr('action'),
+            data: $('#designer-edit-form').serializeArray()
+        }).done(function () {
+            window.location.href = $('#designer-edit-form').attr('action');
+        }).fail(function (error) {
+            var response = error.responseJSON;
+            var $alert = $('#designer-edit-form .alert');
+            var message = '';
+
+            for (var id in response) {
+                message += '<p>' + response[id] + '</p>';
+            }
+
+            $alert.html(message).show().goTo();
+        });
+    });
+
+    // Country and city select, use Select2.
+    $("select#country-select").select2();
+    $("select#city-select").select2();
+
+    // Tag select, use Select2
+    $("select#tag-select").select2({tags: true});
+});
+
+$(function () {
+
+    var $grid = $('#story-list .grid')
+
+    if ($grid.length === 0) {
+        return;
+    }
+
+    $grid.masonry({
+        itemSelector: '.grid-item', // use a separate class for itemSelector, other than .col-
+        columnWidth: '.grid-item',
+        percentPosition: true
+    });
+
+    $grid.find('.story .expand-button').click(function () {
+        $(this).parent('.story').toggleClass('expanded');
+        $(this).siblings('.content').scrollTop(0);
+        $grid.masonry();
+    });
+});
+
+$(function () {
+    $('#picture-carousel').flickity({
+        cellAlign: 'left',
+        freeScroll: true
+    });
+
+    $('#picture-carousel').lightGallery({
+        selector: '.picture-thumb',
+        exThumbImage: 'data-thumb',
+        thumbWidth: 100,
+        thumbContHeight: 120
+    });
+});
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('place-map-draw'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
+
+$(function () {
+    $('#place-map-inner').affix({
+        offset: {
+            top: function () {
+                return this.top = $('#kanibar').outerHeight(true) + 20;
+            },
+            bottom: function () {
+                return this.bottom = $('footer').outerHeight(true) + 20;
+            }
+        }
+    });
+});
 
 //# sourceMappingURL=app.js.map
