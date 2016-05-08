4f12d943cd0a276c9da5787e6d4e3d4513c9494f
Guo Yunhe <guoyunhebrave@gmail.com>
2016-05-08 03:35:38 +0300

    Add WeChat QRcode to footer.

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -10,9 +10,33 @@
                     target="_blank">Facebook</a></li>
                     <li><a href="https://www.instagram.com/santa.kani/"
                     target="_blank">Instagram</a></li>
+                    <li><a href="#" data-toggle="modal" data-target="#wechat-modal">微信</a></li>
                     <li><a href="https://www.facebook.com/groups/270204663311876/"
-                    target="_blank">Forum</a></li>
+                    target="_blank">Group</a></li>
                 </ul>
+                <div class="modal fade" id="wechat-modal" tabindex="-1" role="dialog">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                <h4 class="modal-title">微信公众号</h4>
+                            </div>
+                            <div class="modal-body">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <img src="{{ url('img/qrcode/wechat-tiny-finland.jpg') }}">
+                                    </div>
+                                    <div class="col-sm-6">
+                                        使用微信扫描二维码，关注我们的微信公众号“小芬兰”。
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div><!--.modal-dialog-->
+                </div><!--.modal-->
             </div>
         </div>
         <p class="text-muted">
