911f3c04390fdfd8a784af7dd2c1a9b3dc10837a
Guo Yunhe <guoyunhebrave@gmail.com>
2016-05-08 04:00:08 +0300

    Improve footer layout.

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <p>2015&ndash;{{ date("Y") }} SantaKani.</p>
+                2015&ndash;{{ date("Y") }} SantaKani.
             </div>
             <div class="col-xs-12 col-sm-6">
                 <ul class="list-inline">
@@ -14,36 +14,37 @@
                     <li><a href="https://www.facebook.com/groups/270204663311876/"
                     target="_blank">Group</a></li>
                 </ul>
-                <div class="modal fade" id="wechat-modal" tabindex="-1" role="dialog">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                <h4 class="modal-title">微信公众号</h4>
-                            </div>
-                            <div class="modal-body">
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <img src="{{ url('img/qrcode/wechat-tiny-finland.jpg') }}">
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <p>使用微信扫描二维码，关注我们的微信公众号“小芬兰”。</p>
-                                        <p>微信号: tiny_finland</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                            </div>
-                        </div>
-                    </div><!--.modal-dialog-->
-                </div><!--.modal-->
             </div>
         </div>
-        <p class="text-muted">
+        <div class="text-muted">
             Powered by free software licensed under GNU Affero GPL version 3.
             Source code on <a href="https://github.com/santakani/santakani.com"
             target="_blank">Github</a>.
-        </p>
+        </div>
     </div>
 </footer>
+
+<div class="modal fade" id="wechat-modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">微信公众号</h4>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <img src="{{ url('img/qrcode/wechat-tiny-finland.jpg') }}">
+                    </div>
+                    <div class="col-sm-6">
+                        <p>使用微信扫描二维码，关注我们的微信公众号“小芬兰”。</p>
+                        <p>微信号: tiny_finland</p>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div><!--.modal-dialog-->
+</div><!--.modal-->
