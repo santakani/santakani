24bd5231093e9dae0784c18c9e0fe81576d05bb8
Guo Yunhe <guoyunhebrave@gmail.com>
2016-05-11 07:58:46 +0300

    Add entries to navbar user menu.

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -11,10 +11,20 @@
         @else
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                    {{ Auth::user()->name }} <span class="caret"></span>
+                    <i class="fa fa-plus"></i> <span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-right">
+                    <li><a href="{{ url('/designer/create') }}">New designer</a></li>
+                    <li><a href="{{ url('/place/create') }}">New place</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                    <i class="fa fa-user"></i> <span class="hidden-xs">{{ Auth::user()->name }}</span> <span class="caret"></span>
                 </a>
                 <ul class="dropdown-menu dropdown-menu-right">
                     <li><a href="{{ url('/user/'.Auth::user()->id) }}">Profile</a></li>
+                    <li><a href="{{ url('/help') }}">Help</a></li>
                     <li><a href="{{ url('/setting') }}">Settings</a></li>
                     <li><a href="{{ url('/logout') }}">Logout</a></li>
                 </ul>
