0e0ab160f2989376c2238da9a9a554b2ed693b1d
Guo Yunhe <guoyunhebrave@gmail.com>
2016-05-17 20:43:07 +0300

    Add parent to image table. Remove video supports.

--- database/migrations/2016_02_01_000000_create_image_table.php
+++ database/migrations/2016_02_01_000000_create_image_table.php
@@ -1,7 +1,27 @@
 <?php
 
+/*
+ * This file is part of santakani.com
+ *
+ * (c) Guo Yunhe <guoyunhebrave@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+
+/**
+ * CreateImageTable
+ *
+ * Database migration to create "image" table. This table store metadata of
+ * uploaded images. Current schema only supports images. In future it might
+ * support audio and video if needed.
+ *
+ * @author Guo Yunhe <guoyunhebrave@gmail.com>
+ * @see https://github.com/santakani/santakani.com/wiki/Image
+ */
 
 class CreateImageTable extends Migration
 {
@@ -15,17 +35,29 @@ class CreateImageTable extends Migration
         Schema::create('image', function (Blueprint $table) {
             $table->increments('id');
 
-            $table->string('mime_type'); // image/jpg, video/youtube
+            // MIME type used to validate file content and decide file extension
+            // Support image/jpeg, image/png, image/gif
+            $table->string('mime_type');
+
+            // Original width and height. Detect if it has large and medium sizes
+            // generated.
             $table->integer('width')->unsigned()->nullable();
             $table->integer('height')->unsigned()->nullable();
-            $table->string('external_url')->nullable(); // YouTube, Vimeo URL
-            $table->integer('user_id')->unsigned()->nullable(); // Who uploaded this image or video
+
+            // Who uploaded this image. Users might want to reuse their images.
+            $table->integer('user_id')->unsigned()->nullable();
+
+            // Parent model type and id. Use Laravel Polymorphic relationships
+            // "parent_type" can be "designer", "place", "country", "city"
+            $table->string('parent_type')->nullable();
+            $table->integer('parent_id')->unsigned()->nullable();
 
             // Timestamps
             $table->timestamps();
             $table->softDeletes();
 
-            // When deleted image, set image_id to null
+            // Foreign key
+            // When deleted user, set user_id to null, but still keep the image.
             $table->foreign('user_id')->references('id')->on('user')->onDelete('set null');
         });
     }
