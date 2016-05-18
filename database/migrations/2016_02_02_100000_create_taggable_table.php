aec69e3a8ce0c396493f92bfcdd01dee11ed0a7d
Guo Yunhe <guoyunhebrave@gmail.com>
2016-05-18 20:31:26 +0300

    Rename migration class. #7

--- database/migrations/2016_02_02_100000_create_taggable_table.php
+++ database/migrations/2016_02_02_100000_create_taggable_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTagTable extends Migration
+class CreateTaggableTable extends Migration
 {
     /**
      * Run the migrations.
