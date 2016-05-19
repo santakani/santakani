56ffb7754b4122b1d6fa768a11d9b720243cfa2d
Guo Yunhe <guoyunhebrave@gmail.com>
2016-05-19 18:16:52 +0300

    Clean designer table and update seeds. #8

--- database/migrations/2016_03_01_000000_create_designer_table.php
+++ database/migrations/2016_03_01_000000_create_designer_table.php
@@ -15,11 +15,17 @@ class CreateDesignerTable extends Migration
         Schema::create('designer', function (Blueprint $table) {
             $table->increments('id');
 
-            // Non-translated content
+            // Location
             $table->integer('country_id')->unsigned()->nullable();
             $table->integer('city_id')->unsigned()->nullable();
-            $table->integer('image_id')->unsigned()->nullable(); // Main image
-            $table->integer('user_id')->unsigned()->nullable(); // Owner
+
+            // Cover image
+            $table->integer('image_id')->unsigned()->nullable();
+
+            // Owner
+            $table->integer('user_id')->unsigned()->nullable();
+
+            // Links
             $table->string('email')->nullable();
             $table->string('website')->nullable();
             $table->string('facebook')->nullable();
@@ -31,7 +37,7 @@ class CreateDesignerTable extends Migration
             $table->timestamps();
             $table->softDeletes();
 
-            // Foreign keys, when delete country, city, image, user, set columns to null
+            // Foreign keys
             $table->foreign('country_id')->references('id')->on('country')->onDelete('set null');
             $table->foreign('city_id')->references('id')->on('city')->onDelete('set null');
             $table->foreign('image_id')->references('id')->on('image')->onDelete('set null');
@@ -41,7 +47,7 @@ class CreateDesignerTable extends Migration
         Schema::create('designer_translation', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('designer_id')->unsigned();
-            $table->string('locale')->index();
+            $table->string('locale');
 
             // Translated content
             $table->string('name');
@@ -50,9 +56,10 @@ class CreateDesignerTable extends Migration
 
             $table->timestamps();
 
-            // Unique and foreign key
-            // When deleting designer model, also delete all translation models
+            // Unique
             $table->unique(['designer_id','locale']);
+
+            // Foreign key
             $table->foreign('designer_id')->references('id')->on('designer')->onDelete('cascade');
         });
     }
@@ -65,6 +72,7 @@ class CreateDesignerTable extends Migration
     public function down()
     {
         Schema::drop('designer_translation');
+
         Schema::drop('designer');
     }
 }
--- database/seeds/DesignerTableSeeder.php
+++ database/seeds/DesignerTableSeeder.php
@@ -12,112 +12,42 @@ class DesignerTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('designer')->insert([
-            'country_id' => 1,
-            'city_id' => 1,
-            'image_id' => 13,
-            'user_id' => 1,
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        ]);
+        $designers = [
+            1 => [1, 1, 8, 1, 'Costo'],
+            2 => [1, 1, 13, 1, 'Tanja Kurittu'],
+            3 => [1, 1, 8, 1, 'Costo'],
+            4 => [1, 1, 13, 1, 'Tanja Kurittu'],
+            5 => [1, 1, 8, 1, 'Costo'],
+            6 => [1, 1, 13, 1, 'Tanja Kurittu'],
+        ];
 
-        DB::table('designer_translation')->insert([
-            'designer_id' => 1,
-            'locale' => 'en',
-            'name' => '2OR+BYYAT',
-            'content' => <<<TEXT
-2OR+BYYAT is a young, original and dynamic fashion brand based in Helsinki, Finland. The label is known for skillful unconventional cutting, poetic story of colours and inventive textures as well as high quality leather bags. Products are well made and a lot of attention is paid to details. The key factors of the 2OR+BYYAT philosophy are creativity in symbiosis with wearability. The clothes are eclectic but they stand the test of time; the designs are made to last.
+        $timestamp = Carbon::now()->format('Y-m-d H:i:s');
 
-The highly acclaimed fashion brand was established in Helsinki, Finland, in 2002 by brand’s creative design director YAT.
+        foreach ($designers as $id => $designer) {
+            DB::table('designer')->insert([
+                'country_id' => $designer[0],
+                'city_id' => $designer[1],
+                'image_id' => $designer[2],
+                'user_id' => $designer[3],
+                'email' => 'contact@example.com',
+                'website' => 'http://www.example.com/',
+                'facebook' => 'https://www.facebook.com/',
+                'twitter' => 'https://twitter.com/',
+                'google_plus' => 'https://plus.google.com/',
+                'instagram' => 'https://www.instagram.com/',
+                'created_at' => $timestamp,
+                'updated_at' => $timestamp,
+            ]);
 
-The brand name reflects the fact that in order to create and evolve in the nature, two or more elements are required. No wonder, YAT derives inspiration from the nature for his designs year after year. The brand name is pronounced ʻTwo or plus by YATʼ.
+            DB::table('designer_translation')->insert([
+                'designer_id' => $id,
+                'locale' => 'en',
+                'name' => $designer[4],
+                'content' => file_get_contents('http://loripsum.net/api'),
+                'created_at' => $timestamp,
+                'updated_at' => $timestamp,
+            ]);
+        }
 
-2OR+BYYAT has one flagship store in Fredrikinkatu 35, 00120 Helsinki.
-TEXT
-            ,
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        ]);
-
-        DB::table('designer_translation')->insert([
-            'designer_id' => 1,
-            'locale' => 'zh',
-            'name' => '2OR+BYYAT',
-            'content' => <<<TEXT
-2OR+BYYAT 是一个来自芬兰赫尔辛基的时尚品牌，年轻，富有原创性，动态的。标签是著名的熟练非常规切割，颜色和纹理的发明故事诗意，以及高品质的皮包。产品制作精良，很多重视细节。在2OR+ BYYAT哲学的关键因素是与耐磨性共生的创造力。衣服是不拘一格的，但他们经得起时间的考验;这些设计都是为了持续。
-
-广受赞誉的时尚品牌是由品牌的创意设计总监 YAT 成立于芬兰首都赫尔辛基，于2002年。
-
-品牌名称反映了为了创建和在自然界中进化，需要两个或更多个元件的事实。难怪，逸年复一年衍生灵感来自大自然，他的设计。品牌名称是由YAT'明显'Two或加。
-
-2OR+ BYYAT 的旗舰店位于 Fredrikinkatu 35，00120 赫尔辛基。
-TEXT
-            ,
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        ]);
-
-        DB::table('designer')->insert([
-            'image_id' => 2,
-            'user_id' => 2,
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        ]);
-
-         DB::table('designer_translation')->insert([
-            'designer_id' => 2,
-            'locale' => 'en',
-            'name' => 'Du Yuexin',
-            'content' => file_get_contents('http://loripsum.net/api'),
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        ]);
-
-        DB::table('designer_translation')->insert([
-            'designer_id' => 2,
-            'locale' => 'zh',
-            'name' => '杜玥辛',
-            'content' => file_get_contents('http://loripsum.net/api'),
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        ]);
-
-        DB::table('designer')->insert([
-            'image_id' => 3,
-            'user_id' => 3,
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        ]);
-
-         DB::table('designer_translation')->insert([
-            'designer_id' => 3,
-            'locale' => 'en',
-            'name' => 'Yun Xiaotong',
-            'content' => file_get_contents('http://loripsum.net/api'),
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        ]);
-
-        DB::table('designer_translation')->insert([
-            'designer_id' => 3,
-            'locale' => 'zh',
-            'name' => '云小童',
-            'content' => file_get_contents('http://loripsum.net/api'),
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        ]);
-
-        DB::table('designer')->insert([
-            'image_id' => 4,
-            'user_id' => 1,
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        ]);
-
-         DB::table('designer_translation')->insert([
-            'designer_id' => 4,
-            'locale' => 'en',
-            'name' => 'Yu Huiyang',
-            'content' => file_get_contents('http://loripsum.net/api'),
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        ]);
-
-        DB::table('designer_translation')->insert([
-            'designer_id' => 4,
-            'locale' => 'zh',
-            'name' => '余慧阳',
-            'content' => file_get_contents('http://loripsum.net/api'),
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-        ]);
     }
 }
