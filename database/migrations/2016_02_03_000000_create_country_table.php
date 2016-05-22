a47125c1e5c9bda9eb18925a499b163c0a499c04
Guo Yunhe <guoyunhebrave@gmail.com>
2016-05-22 11:31:56 +0300

    Add geo information to country table. #14

--- database/migrations/2016_02_03_000000_create_country_table.php
+++ database/migrations/2016_02_03_000000_create_country_table.php
@@ -16,13 +16,19 @@ class CreateCountryTable extends Migration
             $table->increments('id');
 
             // Name in URL, lower-case. e.g. "finland", "germany"
-            $table->string('slug')->unique();
+            $table->string('slug', 50)->unique();
 
             // Country code, ISO 3166-1 alpha-2, upper-case. e.g. "FI", "DE"
-            $table->string('code')->unique();
+            $table->string('code', 2)->unique();
 
             // Icon/cover image
             $table->integer('image_id')->unsigned()->nullable();
+
+            // Region/continent
+            $table->string('region', 10)->nullable();
+
+            // Subregion
+            $table->string('subregion', 20)->nullable();
 
             // Timestamps
             $table->timestamps();
@@ -32,10 +38,18 @@ class CreateCountryTable extends Migration
             $table->foreign('image_id')->references('id')->on('image')->onDelete('set null');
         });
 
+        // MySQL/MariaDB GEOMETRY, which is not supported by Laravel Blueprint
+        DB::statement('ALTER TABLE country ADD coordinate POINT AFTER subregion');
+        DB::statement('ALTER TABLE country ADD border MULTIPOLYGON AFTER coordinate');
+
         Schema::create('country_translation', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('country_id')->unsigned();
-            $table->string('locale');
+            // ISO 639-1 codes (2 letters)
+            $table->string('locale', 2);
+
+            // Flags
+            $table->boolean('native');
 
             // Translated content
             $table->string('name');
