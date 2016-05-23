7718a9d33895381ab21783b0b06e82d19ab79bea
Guo Yunhe <guoyunhebrave@gmail.com>
2016-05-23 21:26:04 +0300

    Comment city table.

--- database/migrations/2016_02_04_000000_create_city_table.php
+++ database/migrations/2016_02_04_000000_create_city_table.php
@@ -1,8 +1,25 @@
 <?php
 
+/*
+ * This file is part of santakani.com
+ *
+ * (c) Guo Yunhe <guoyunhebrave@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
+/**
+ * CreateCityTable
+ *
+ * Database migration to create "city" and "city_translation" table.
+ *
+ * @author Guo Yunhe <guoyunhebrave@gmail.com>
+ * @see https://github.com/santakani/santakani.com/wiki/City
+ */
 class CreateCityTable extends Migration
 {
     /**
@@ -15,26 +32,16 @@ class CreateCityTable extends Migration
         Schema::create('city', function (Blueprint $table) {
             $table->increments('id');
 
-            // Name in URL, lower-case. e.g. "helsinki", "beijing"
-            $table->string('slug', 50);
-
-            // Country id
+            $table->string('slug');
             $table->integer('country_id')->unsigned()->nullable();
-
-            // Icon/cover image
             $table->integer('image_id')->unsigned()->nullable();
+            $table->string('timezone'); // "Europe/Helsinki", "Asia/Shanghai"
 
-            // Timezone, like "Europe/Helsinki", "Asia/Shanghai"
-            $table->string('timezone', 30);
-
-            // Timestamps
             $table->timestamps();
             $table->softDeletes();
 
-            // Unique
             $table->unique(['country_id', 'slug']);
 
-            // Foreign keys
             $table->foreign('country_id')->references('id')->on('country')->onDelete('set null');
             $table->foreign('image_id')->references('id')->on('image')->onDelete('set null');
         });
@@ -45,25 +52,16 @@ class CreateCityTable extends Migration
         Schema::create('city_translation', function (Blueprint $table) {
             $table->increments('id');
 
-            // Parent
             $table->integer('city_id')->unsigned();
+            $table->string('locale', 2); // ISO 639-1 (2 letters)
 
-            // ISO 639-1 codes (2 letters)
-            $table->string('locale', 2);
-
-            // Name
             $table->string('name')->nullable();
+            $table->text('content')->nullable(); // HTML
 
-            // Page content
-            $table->text('content')->nullable();
-
-            // Timestamps
             $table->timestamps();
 
-            // Unique
             $table->unique(['city_id','locale']);
 
-            // Foreign key
             $table->foreign('city_id')->references('id')->on('city')->onDelete('cascade');
         });
     }
