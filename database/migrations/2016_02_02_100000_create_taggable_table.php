7de968872124ff9d42a8a36eff6f93f9dbbd629d
Guo Yunhe <guoyunhebrave@gmail.com>
2016-05-23 21:19:06 +0300

    Comment taggable table migration.

--- database/migrations/2016_02_02_100000_create_taggable_table.php
+++ database/migrations/2016_02_02_100000_create_taggable_table.php
@@ -1,8 +1,26 @@
 <?php
 
+/*
+ * This file is part of santakani.com
+ *
+ * (c) Guo Yunhe <guoyunhebrave@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
+/**
+ * CreateTaggableTable
+ *
+ * Database migration to create "taggable" table. Control relationship between
+ * tags and taggable objects.
+ *
+ * @author Guo Yunhe <guoyunhebrave@gmail.com>
+ * @see https://github.com/santakani/santakani.com/wiki/Tag
+ */
 class CreateTaggableTable extends Migration
 {
     /**
@@ -14,13 +32,12 @@ class CreateTaggableTable extends Migration
     {
         Schema::create('taggable', function (Blueprint $table) {
             $table->integer('tag_id')->unsigned();
-            $table->string('taggable_type');
+
+            $table->string('taggable_type'); // Polymorphic relatioship, many to many.
             $table->integer('taggable_id')->unsigned();
 
-            // Unique
             $table->unique(['tag_id', 'taggable_type', 'taggable_id']);
 
-            // Foreign key
             $table->foreign('tag_id')->references('id')->on('tag')->onDelete('cascade');
         });
     }
