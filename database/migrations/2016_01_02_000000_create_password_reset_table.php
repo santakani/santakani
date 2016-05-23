ea8cbb9ac3928cdf76af2ec1eb358e192add44fc
Guo Yunhe <guoyunhebrave@gmail.com>
2016-05-23 21:03:59 +0300

    Comment password_reset table migration.

--- database/migrations/2016_01_02_000000_create_password_reset_table.php
+++ database/migrations/2016_01_02_000000_create_password_reset_table.php
@@ -1,8 +1,26 @@
 <?php
 
+/*
+ * This file is part of santakani.com
+ *
+ * (c) Guo Yunhe <guoyunhebrave@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
+/**
+ * CreatePasswordResetTable
+ *
+ * Database migration to create "password_reset" table. A record will be created
+ * where user request a password reset line through email.
+ *
+ * @author Guo Yunhe <guoyunhebrave@gmail.com>
+ * @see https://github.com/santakani/santakani.com/wiki/User
+ */
 class CreatePasswordResetTable extends Migration
 {
     /**
