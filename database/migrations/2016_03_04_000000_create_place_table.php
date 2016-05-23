2122df37679cf038bb0f7c18a63863ce053a9a11
Guo Yunhe <guoyunhebrave@gmail.com>
2016-05-23 22:16:12 +0300

    Comment place table.

--- database/migrations/2016_03_04_000000_create_place_table.php
+++ database/migrations/2016_03_04_000000_create_place_table.php
@@ -1,8 +1,25 @@
 <?php
 
+/*
+ * This file is part of santakani.com
+ *
+ * (c) Guo Yunhe <guoyunhebrave@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
+/**
+ * CreatePlaceTable
+ *
+ * Database migration to create "place" and "place_translation" table.
+ *
+ * @author Guo Yunhe <guoyunhebrave@gmail.com>
+ * @see https://github.com/santakani/santakani.com/wiki/Place
+ */
 class CreatePlaceTable extends Migration
 {
     /**
@@ -15,57 +32,45 @@ class CreatePlaceTable extends Migration
         Schema::create('place', function (Blueprint $table) {
             $table->increments('id');
 
-            // Location
+            $table->string('type'); // shop, showroom, studio, school, etc.
+
             $table->integer('country_id')->unsigned()->nullable();
             $table->integer('city_id')->unsigned()->nullable();
-            // POINT `location` is added by raw SQL, since not supported by Laravel
-            // `address` is in translation table
-
-            // Cover image
             $table->integer('image_id')->unsigned()->nullable();
-
-            // Owner
             $table->integer('user_id')->unsigned()->nullable();
 
-            // Place type: shop, showroom, studio, school, etc.
-            $table->string('type');
+            $table->string('address');
 
-            // Contacts & links
             $table->string('email');
             $table->string('phone');
             $table->string('website');
             $table->string('facebook'); // Facebook page
             $table->string('google_plus'); // Google+ business page
 
-            // Timestamps
             $table->timestamps();
             $table->softDeletes();
 
-            // Foreign keys, when delete country, city, image, user, set columns to null
             $table->foreign('country_id')->references('id')->on('country')->onDelete('set null');
             $table->foreign('city_id')->references('id')->on('city')->onDelete('set null');
             $table->foreign('image_id')->references('id')->on('image')->onDelete('set null');
             $table->foreign('user_id')->references('id')->on('user')->onDelete('set null');
         });
 
-        // MySQL/MariaDB POINT `location`
-        DB::statement('ALTER TABLE place ADD location POINT AFTER city_id' );
+        DB::statement('ALTER TABLE place ADD coordinate POINT AFTER address' );
 
         Schema::create('place_translation', function (Blueprint $table) {
             $table->increments('id');
+
             $table->integer('place_id')->unsigned();
             $table->string('locale');
 
-            // Translated content
             $table->string('name');
             $table->text('content');
-            $table->string('address');
 
             $table->timestamps();
 
-            // Unique and foreign key
-            // When deleting city model, also delete all translation models
             $table->unique(['place_id','locale']);
+
             $table->foreign('place_id')->references('id')->on('place')->onDelete('cascade');
         });
     }
