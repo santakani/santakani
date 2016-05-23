fbb5bb3be5ae1531a6e902e138a93840530b430e
Guo Yunhe <guoyunhebrave@gmail.com>
2016-05-23 20:53:17 +0300

    Comment user table migration.

--- database/migrations/2016_01_01_000000_create_user_table.php
+++ database/migrations/2016_01_01_000000_create_user_table.php
@@ -1,7 +1,26 @@
 <?php
 
+/*
+ * This file is part of santakani.com
+ *
+ * (c) Guo Yunhe <guoyunhebrave@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+
+/**
+ * CreateUserTable
+ *
+ * Database migration to create "user" table. This table control user auth and
+ * settings.
+ *
+ * @author Guo Yunhe <guoyunhebrave@gmail.com>
+ * @see https://github.com/santakani/santakani.com/wiki/User
+ */
 
 class CreateUserTable extends Migration
 {
@@ -14,10 +33,14 @@ class CreateUserTable extends Migration
     {
         Schema::create('user', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+
+            $table->string('name'); // Display name, not unique, not used for login
+            $table->text('description'); // Plain text
+
+            $table->string('email')->unique(); // As username used for login
             $table->string('password', 60);
             $table->rememberToken();
+
             $table->timestamps();
             $table->softDeletes();
         });
