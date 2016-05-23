3e3294b32b863d67857a40b36fcd5e187060bbba
Guo Yunhe <guoyunhebrave@gmail.com>
2016-05-23 14:09:10 +0300

    Import cities.

--- database/migrations/2016_02_04_000000_create_city_table.php
+++ database/migrations/2016_02_04_000000_create_city_table.php
@@ -15,8 +15,8 @@ class CreateCityTable extends Migration
         Schema::create('city', function (Blueprint $table) {
             $table->increments('id');
 
-            // Name in URL, lower-case. e.g. "finland", "germany"
-            $table->string('slug')->unique();
+            // Name in URL, lower-case. e.g. "helsinki", "beijing"
+            $table->string('slug', 50);
 
             // Country id
             $table->integer('country_id')->unsigned()->nullable();
@@ -24,23 +24,38 @@ class CreateCityTable extends Migration
             // Icon/cover image
             $table->integer('image_id')->unsigned()->nullable();
 
+            // Timezone, like "Europe/Helsinki", "Asia/Shanghai"
+            $table->string('timezone', 30);
+
             // Timestamps
             $table->timestamps();
             $table->softDeletes();
+
+            // Unique
+            $table->unique(['country_id', 'slug']);
 
             // Foreign keys
             $table->foreign('country_id')->references('id')->on('country')->onDelete('set null');
             $table->foreign('image_id')->references('id')->on('image')->onDelete('set null');
         });
 
+        // MySQL/MariaDB GEOMETRY, which is not supported by Laravel Blueprint
+        DB::statement('ALTER TABLE city ADD coordinate POINT AFTER image_id');
+
         Schema::create('city_translation', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('city_id')->unsigned();
-            $table->string('locale');
 
-            // Translated content
-            $table->string('name');
-            $table->text('content');
+            // Parent
+            $table->integer('city_id')->unsigned();
+
+            // ISO 639-1 codes (2 letters)
+            $table->string('locale', 2);
+
+            // Name
+            $table->string('name')->nullable();
+
+            // Page content
+            $table->text('content')->nullable();
 
             // Timestamps
             $table->timestamps();
--- database/migrations/2016_05_23_083520_import_cities.php
+++ database/migrations/2016_05_23_083520_import_cities.php
@@ -1,5 +1,7 @@
 <?php
 
+use Carbon\Carbon;
+use Cocur\Slugify\Slugify;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
@@ -12,7 +14,63 @@ class ImportCities extends Migration
      */
     public function up()
     {
-        //
+        echo "Import cities start\n";
+
+        $slugify = new Slugify();
+
+        $handle = fopen(base_path("database/sources/cities15000.txt"), "r");
+        if ($handle) {
+            $id = 1;
+            while (($city = fgetcsv($handle, 0, "\t")) !== false) {
+
+                if (count($city) !== 19) {
+                    echo "\tSkip: $city[1]\n";
+                    continue;
+                }
+
+                $x = $city[4];
+                $y = $city[5];
+                $coordinate = "PointFromText('POINT($x $y)')";
+
+                $country = DB::table('country')->where('code', $city[8])->first();
+                if (isset($country->id)) {
+                    $country_id = $country->id;
+                } else {
+                    echo "\tSkip: $city[1]\n";
+                    continue;
+                }
+
+                $slug = $this->slug2($slugify->slugify($city[2]), $country_id);
+
+                DB::table('city')->insert([
+                    'slug' => $slug,
+                    'country_id' => $country_id,
+                    'coordinate' => $coordinate,
+                    'timezone' => $city[17],
+                    'coordinate' => DB::raw($coordinate),
+                    'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                ]);
+
+                DB::table('city_translation')->insert([
+                    'city_id' => $id,
+                    'locale' => 'en',
+                    'name' => $city[1],
+                    'content' => $city[3],
+                    'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                    'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                ]);
+
+                $id++;
+            }
+
+            fclose($handle);
+        } else {
+            // error opening the file.
+            echo "\tError: Cannot open file 'database/sources/cities15000.txt'\n";
+        }
+
+        echo "Import cities end\n";
     }
 
     /**
@@ -22,6 +80,30 @@ class ImportCities extends Migration
      */
     public function down()
     {
-        //
+        // Cannot undo
+    }
+
+    public function slug2($slug, $country_id, $n = 0)
+    {
+        if ($n === 0) {
+            $cities = DB::table('city')->where([
+                ['slug', $slug],
+                ['country_id', $country_id],
+            ])->get();
+        } else {
+            $cities = DB::table('city')->where([
+                ['slug', $slug . '-' . $n],
+                ['country_id', $country_id],
+            ])->get();
+        }
+
+        if (count($cities)) {
+            $n++;
+            return $this->slug2($slug, $country_id, $n);
+        } elseif ($n === 0) {
+            return $slug;
+        } else {
+            return $slug . '-' . $n;
+        }
     }
 }
