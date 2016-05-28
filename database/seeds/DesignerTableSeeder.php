e464d6c4d3ff83621ba878ab5965c7fb9da62e86
Guo Yunhe <guoyunhebrave@gmail.com>
2016-05-28 14:11:14 +0300

    Make more test data of designer and place table.

--- database/seeds/DesignerTableSeeder.php
+++ database/seeds/DesignerTableSeeder.php
@@ -12,6 +12,10 @@
 use Illuminate\Database\Seeder;
 use Carbon\Carbon;
 
+use App\City;
+use App\Image;
+use App\User;
+
 /**
  * DesignerTableSeeder
  *
@@ -30,41 +34,45 @@ class DesignerTableSeeder extends Seeder
      */
     public function run()
     {
-        $designers = [
-            1 => [1, 1, 8, 1, 'Costo'],
-            2 => [1, 1, 13, 1, 'Tanja Kurittu'],
-            3 => [1, 1, 8, 1, 'Costo'],
-            4 => [1, 1, 13, 1, 'Tanja Kurittu'],
-            5 => [1, 1, 8, 1, 'Costo'],
-            6 => [1, 1, 13, 1, 'Tanja Kurittu'],
-        ];
+        $cities = [];
 
-        $timestamp = Carbon::now()->format('Y-m-d H:i:s');
+        $cities[] = City::where('slug', 'helsinki')->first();
+        $cities[] = City::where('slug', 'lahti')->first();
+        $cities[] = City::where('slug', 'espoo')->first();
+        $cities[] = City::where('slug', 'vantaa')->first();
+        $cities[] = City::where('slug', 'tampere')->first();
 
-        foreach ($designers as $id => $designer) {
-            DB::table('designer')->insert([
-                'country_id' => $designer[0],
-                'city_id' => $designer[1],
-                'image_id' => $designer[2],
-                'user_id' => $designer[3],
+        $images = Image::all();
+
+        $users = User::all();
+
+        for ($i = 0; $i < 100; $i++) {
+            $city = $cities[rand(0, count($cities)-1)];
+            $image = $images[$i%count($images)];
+            $user = $users[rand(0, count($users)-1)];
+
+            $id = DB::table('designer')->insertGetId([
+                'city_id' => $city->id,
+                'image_id' => $image->id,
+                'user_id' => $user->id,
                 'email' => 'contact@example.com',
                 'website' => 'http://www.example.com/',
                 'facebook' => 'https://www.facebook.com/',
                 'twitter' => 'https://twitter.com/',
                 'google_plus' => 'https://plus.google.com/',
                 'instagram' => 'https://www.instagram.com/',
-                'created_at' => $timestamp,
-                'updated_at' => $timestamp,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ]);
 
             DB::table('designer_translation')->insert([
                 'designer_id' => $id,
                 'locale' => 'en',
-                'name' => $designer[4],
+                'name' => 'Test Designer ' . $id,
                 'tagline' => 'Good typeface make any text easier to read',
                 'content' => file_get_contents('http://loripsum.net/api'),
-                'created_at' => $timestamp,
-                'updated_at' => $timestamp,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ]);
         }
 
--- database/seeds/PlaceTableSeeder.php
+++ database/seeds/PlaceTableSeeder.php
@@ -12,6 +12,10 @@
 use Illuminate\Database\Seeder;
 use Carbon\Carbon;
 
+use App\City;
+use App\Image;
+use App\User;
+
 /**
  * PlaceTableSeeder
  *
@@ -30,47 +34,50 @@ class PlaceTableSeeder extends Seeder
      */
     public function run()
     {
-        $places = [
-            1 => [1, 1, 1, 1, 'Costo'],
-            2 => [1, 1, 23, 1, 'folklore'],
-            3 => [1, 1, 1, 1, 'Costo'],
-            4 => [1, 1, 23, 1, 'folklore'],
-            5 => [1, 1, 1, 1, 'Costo'],
-            6 => [1, 1, 23, 1, 'folklore'],
-            7 => [1, 1, 1, 1, 'Costo'],
-            8 => [1, 1, 23, 1, 'folklore'],
-            9 => [1, 1, 1, 1, 'Costo'],
-            10 => [1, 1, 23, 1, 'folklore'],
-        ];
+        $cities = [];
 
-        $timestamp = Carbon::now()->format('Y-m-d H:i:s');
+        $cities[] = City::where('slug', 'helsinki')->first();
+        $cities[] = City::where('slug', 'lahti')->first();
+        $cities[] = City::where('slug', 'espoo')->first();
+        $cities[] = City::where('slug', 'vantaa')->first();
+        $cities[] = City::where('slug', 'tampere')->first();
 
-        foreach ($places as $id => $place) {
-            DB::table('place')->insert([
-                'country_id' => $place[0],
-                'city_id' => $place[1],
-                'image_id' => $place[2],
-                'user_id' => $place[3],
+        $images = Image::all();
+
+        $users = User::all();
+
+        $types = ['shop', 'studio', 'showroom', 'museum', 'school'];
+
+        for ($i = 0; $i < 100; $i++) {
+            $city = $cities[rand(0, count($cities)-1)];
+            $image = $images[$i%count($images)];
+            $user = $users[rand(0, count($users)-1)];
+            $type = $types[rand(0, count($types)-1)];
+
+            $id = DB::table('place')->insertGetId([
+                'city_id' => $city->id,
+                'image_id' => $image->id,
+                'user_id' => $user->id,
                 'type' => 'store',
                 'address' => 'Servinkuja 1 B 19',
-                'latitude' => 60.1686973 + rand(0, 1000) / 20000 - 0.025,
-                'longitude' => 24.9512867 + rand(0, 1000) / 20000 - 0.025,
+                'latitude' => $city->latitude + rand(0, 1000) / 20000 - 0.025,
+                'longitude' => $city->longitude + rand(0, 1000) / 20000 - 0.025,
                 'phone' => '+3581234567',
                 'email' => 'contact@example.com',
                 'website' => 'http://www.example.com/',
                 'facebook' => 'https://www.facebook.com/',
                 'google_plus' => 'https://plus.google.com/',
-                'created_at' => $timestamp,
-                'updated_at' => $timestamp,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ]);
 
             DB::table('place_translation')->insert([
                 'place_id' => $id,
                 'locale' => 'en',
-                'name' => $place[4],
+                'name' => 'Test ' . $type . ' ' . $id,
                 'content' => file_get_contents('http://loripsum.net/api'),
-                'created_at' => $timestamp,
-                'updated_at' => $timestamp,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ]);
         }
 
