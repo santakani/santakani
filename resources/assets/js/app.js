068de54285a202209854060f80977e49ad389024
Guo Yunhe <guoyunhebrave@gmail.com>
2016-05-28 21:55:30 +0300

    Clean app.js.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,16 +4,8 @@
 
 (function () {
 
-    var npmPath = '../../../node_modules';
-
-    // Libraries
-    require('openlayers');
-
-    // jQuery plugins
+    // Load jQuery plugins
     require('./jquery/go-to');
-
-    // Utilities
-    require('./utility/template');
 
     // Pages
     require('./page/designer/create');
