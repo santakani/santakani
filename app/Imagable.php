7eb35543468652b789554b3e2f6e90977dfa9d4e
Guo Yunhe <guoyunhebrave@gmail.com>
2016-06-02 17:11:33 +0300

    Add gallery setter and getter. #26

--- app/Imagable.php
+++ app/Imagable.php
@@ -43,6 +43,22 @@ trait Imagable {
     }
 
     /**
+     * "image_ids" getter.
+     *
+     * @return int[]
+     */
+    public function getImageIdsAttribute()
+    {
+        $image_ids = [];
+
+        foreach ($this->images as $image) {
+            $image_ids[] = $image->id;
+        }
+
+        return $image_ids;
+    }
+
+    /**
      * "gallery_images" getter.
      *
      * @return \Illuminate\Database\Eloquent\Relations\Relation
@@ -50,6 +66,47 @@ trait Imagable {
     public function getGalleryImagesAttribute()
     {
         return $this->images()->where('weight', '>', 0)->get();
+    }
+
+    /**
+     * "gallery_image_ids" setter.
+     *
+     * @param int[] image_ids Ordered list of image ids.
+     */
+    public function setGalleryImageIdsAttribute($image_ids)
+    {
+        $image_ids = array_intersect($image_ids, $this->image_ids);
+        $hide_image_ids = array_diff($this->image_ids, $image_ids);
+
+        $weight = 1;
+        foreach ($image_ids as $image_id) {
+            $image = Image::find($image_id);
+            $image->weight = $weight;
+            $image->save();
+            $weight++;
+        }
+
+        foreach ($hide_image_ids as $image_id) {
+            $image = Image::find($image_id);
+            $image->weight = null;
+            $image->save();
+        }
+    }
+
+    /**
+     * "gallery_image_ids" getter.
+     *
+     * @return int[]
+     */
+    public function getGalleryImageIdsAttribute()
+    {
+        $image_ids = [];
+
+        foreach ($this->gallery_images as $image) {
+            $image_ids[] = $image->id;
+        }
+
+        return $image_ids;
     }
 
 }
