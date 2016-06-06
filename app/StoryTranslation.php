c913728396d983ff6684a0e5dc8b9c7a8313306f
Guo Yunhe <guoyunhebrave@gmail.com>
2016-06-06 14:32:44 +0300

    StoryTranslation model. #15

---
+++ app/StoryTranslation.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of santakani.com
+ *
+ * (c) Guo Yunhe <guoyunhebrave@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * StoryTranslation
+ *
+ * Model for story translation.
+ *
+ * @author Guo Yunhe <guoyunhebrave@gmail.com>
+ * @see https://github.com/santakani/santakani.com/wiki/Story
+ */
+class StoryTranslation extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'story_translation';
+}
