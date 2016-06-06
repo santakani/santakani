a086f1d4d97403e4b7d55dd3374f83fb821417f4
Guo Yunhe <guoyunhebrave@gmail.com>
2016-06-06 09:56:12 +0300

    Table story and translation. #15

--- database/migrations/2016_06_06_061611_create_story_table.php
+++ database/migrations/2016_06_06_061611_create_story_table.php
@@ -1,8 +1,25 @@
 <?php
 
+/*
+ * This file is part of santakani.com
+ *
+ * (c) Guo Yunhe <guoyunhebrave@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
+/**
+ * CreateStoryTable
+ *
+ * Database migration to create "story" and "story_translation" table.
+ *
+ * @author Guo Yunhe <guoyunhebrave@gmail.com>
+ * @see https://github.com/santakani/santakani.com/wiki/Story
+ */
 class CreateStoryTable extends Migration
 {
     /**
@@ -14,8 +31,35 @@ class CreateStoryTable extends Migration
     {
         Schema::create('story', function (Blueprint $table) {
             $table->increments('id');
+
+            $table->integer('image_id')->unsigned()->nullable();
+            $table->integer('user_id')->unsigned()->nullable();
+
             $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('image_id')->references('id')->on('image')->onDelete('set null');
+            $table->foreign('user_id')->references('id')->on('user')->onDelete('set null');
         });
+
+        Schema::create('story_translation', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('story_id')->unsigned();
+            $table->string('locale');
+
+            $table->string('title');
+            $table->text('content');
+
+            $table->timestamps();
+
+            $table->unique(['story_id','locale']);
+
+            $table->foreign('story_id')->references('id')->on('story')->onDelete('cascade');
+        });
+
+        DB::statement('ALTER TABLE story_translation ADD FULLTEXT INDEX story_translation_title_ft_index(title)');
+        DB::statement('ALTER TABLE story_translation ADD FULLTEXT INDEX story_translation_content_ft_index(content)');
     }
 
     /**
@@ -25,6 +69,8 @@ class CreateStoryTable extends Migration
      */
     public function down()
     {
+        Schema::drop('story_translation');
+
         Schema::drop('story');
     }
 }
