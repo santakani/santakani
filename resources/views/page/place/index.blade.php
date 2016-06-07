57eaceecf558eef84549ef68d1f6c21d63bae471
Guo Yunhe <guoyunhebrave@gmail.com>
2016-06-07 13:28:24 +0300

    Redesign place index page.

--- resources/views/page/place/index.blade.php
+++ resources/views/page/place/index.blade.php
@@ -13,34 +13,33 @@
 </div>
 
 <div id="place-list">
-    @foreach ($places as $place)
-        <article id="place-{{ $place->id }}" class="place"
-            data-id="{{ $place->id }}" data-latitude="{{ $place->latitude }}"
-            data-longitude="{{ $place->longitude }}">
-            <div class="image" style="background-image:url({{ $place->image?$place->image->file_urls['thumb']:'http://placehold.it/200x200?text=NO+IMAGE' }})">
-                <span class="type">{{ $place->type }}</span>
-            </div>
-            <div class="text">
-                <h3 class="title">
-                    <span class="dot"></span>
-                    <a href="{{ $place->url }}">
-                        {{ $place->text('name') }}
-                    </a>
-                    <small>
-                        <i class="fa fa-heart-o"></i> 59
-                        <i class="fa fa-comment-o"></i> 14
-                    </small>
-                </h3>
-                @include('component.tag-list', [
-                    'tags' => $place->tags,
-                    'style' => 'round',
-                ])
-                <div class="content">
-                    {!! $place->text('content') !!}
-                </div>
-            </div>
-        </article>
-    @endforeach
-    {!! $places->links() !!}
+    <div class="container-fluid">
+        @foreach ($places as $place)
+            <article id="place-{{ $place->id }}" class="place material-card"
+                data-id="{{ $place->id }}" data-latitude="{{ $place->latitude }}"
+                data-longitude="{{ $place->longitude }}">
+                <a href="{{ $place->url }}">
+                    <div class="cover-image" style="background-image:url({{ $place->image?$place->image->file_urls['medium']:'' }})">
+                        <span class="type">{{ $place->type }}</span>
+                    </div>
+                    <div class="text">
+                        <h1 class="name">
+                            <span class="dot"></span>
+                            {{ $place->text('name') }}
+                            <small>
+                                {{ $place->address }}
+                            </small>
+                        </h1>
+                        <div class="excerpt">
+                            {{ $place->excerpt('content') }}
+                        </div>
+                    </div>
+                </a>
+            </article>
+        @endforeach
+    </div>
+    <div class="text-center">
+        {!! $places->links() !!}
+    </div>
 </div><!-- #place-list -->
 @endsection
