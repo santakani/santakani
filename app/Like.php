d01beb25a527d4413996b5352f6411b454d840ab
Guo Yunhe <guoyunhebrave@gmail.com>
2016-06-14 18:11:05 +0300

    Like model and LikeFeature trait.

--- app/Like.php
+++ app/Like.php
@@ -1,10 +1,42 @@
 <?php
 
+/*
+ * This file is part of santakani.com
+ *
+ * (c) Guo Yunhe <guoyunhebrave@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Like
+ *
+ * Model for like.
+ *
+ * @author Guo Yunhe <guoyunhebrave@gmail.com>
+ * @see https://github.com/santakani/santakani.com/wiki/Like
+ */
 class Like extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'like';
+
+
+    //==========================================================================
+    // Relationship Methods
+    //==========================================================================
+
+    public function likeable()
+    {
+        return $this->morphTo();
+    }
 }
---
+++ app/LikeFeature.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of santakani.com
+ *
+ * (c) Guo Yunhe <guoyunhebrave@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App;
+
+/**
+ * LikeFeature
+ *
+ * Functions to get and set relationships with Like model.
+ *
+ * @author Guo Yunhe <guoyunhebrave@gmail.com>
+ * @see https://github.com/santakani/santakani.com/wiki/Like
+ */
+
+trait LikeFeature {
+    /**
+     * Likes assigned to this page/model.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\Relation
+     */
+    public function likes()
+    {
+        return $this->morphMany('App\Like', 'likeable');
+    }
+
+    /**
+     * Count likes again and save to database.
+     */
+    public function updateLikeCount()
+    {
+        $this->like_count = $this->likes()->count();
+        $this->save();
+    }
+}
