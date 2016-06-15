3ad8b4915f3591cd538609121897da4eda2369b1
Guo Yunhe <guoyunhebrave@gmail.com>
2016-06-15 09:26:49 +0300

    Change <a> to <button>

--- resources/views/page/story/show.blade.php
+++ resources/views/page/story/show.blade.php
@@ -14,17 +14,17 @@
             </h1>
             <div class="action-buttons">
                 <?php $liked = $story->liked; ?>
-                <a id="like-button" class="btn btn-default" href="#"
+                <button type="button" id="like-button" class="btn btn-default"
                     data-likeable-id="{{ $story->id}}" data-likeable-type="story"
                     data-liked="{{ $liked }}">
                     <i class="fa fa-lg {{ $liked?'fa-heart':'fa-heart-o' }}"></i>
                     Like
                     <span class="badge">{{ $story->like_count }}</span>
-                </a>
+                </button>
                 @if ($can_edit)
                     <a id="edit-button" class="btn btn-default" href="{{ $story->url . '/edit' }}">
                         <i class="fa fa-pencil-square-o fa-lg"></i> Edit</a>
-                    <a id="delete-button" class="btn btn-danger" href="#">
+                    <button type="button" id="delete-button" class="btn btn-danger">
                         <i class="fa fa-trash-o fa-lg"></i> Delete</a>
                 @endif
             </div>
