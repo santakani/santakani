48f73bbc562c631bdb6d3513abddb8de587846f8
Guo Yunhe <guoyunhebrave@gmail.com>
2016-06-15 09:23:45 +0300

    Delete button JS module.

--- resources/assets/js/page/story/show.js
+++ resources/assets/js/page/story/show.js
@@ -1,6 +1,5 @@
-var swal = require('sweetalert');
-
 var LikeButton = require('../../view/like-button');
+var DeleteButton = require('../../view/delete-button');
 
 $(function () {
 
@@ -9,40 +8,12 @@ $(function () {
     }
 
     // Action bar starts
-    $('#delete-button').click(function (e) {
-        e.preventDefault();
-
-        swal({
-            title: "Confirm Delete",
-            text: 'Deleted page can be restored in your <a href="/setting">account setting</a> page.',
-            type: "warning",
-            html: true,
-            showCancelButton: true,
-            closeOnConfirm: false,
-            allowOutsideClick: true,
-            showLoaderOnConfirm: true,
-            confirmButtonColor: "#DD6B55",
-            confirmButtonText: "Yes, delete it!",
-            cancelButtonText: "No, cancel please!",
-        },
-        function(){
-            $.ajax({
-                url: location.pathname,
-                method: 'delete',
-                data: {
-                    _token: csrfToken
-                }
-            }).done(function () {
-                swal("Deleted");
-            }).fail(function () {
-                swal("Cannot delete this page.");
-            });
-        });
+    var deleteButton = new DeleteButton({
+        el: '#delete-button',
     });
 
     var likeButton = new LikeButton({
         el: '#like-button',
     });
-
     // Action bar ends
 });
---
+++ resources/assets/js/view/delete-button.js
@@ -0,0 +1,54 @@
+var Backbone = require('backbone');
+var swal = require('sweetalert');
+
+module.exports = Backbone.View.extend({
+
+    el: '#delete-button',
+
+    url: location.pathname,
+
+    redirect: '/', // Set false if do not want redirect after delete.
+
+    events: {
+        'click': 'delete',
+    },
+
+    initialize: function (options) {
+        _.extend(this, _.pick(options, 'url', 'redirect'));
+    },
+
+    delete: function () {
+        var that = this;
+        swal({
+            title: "Confirm Delete",
+            text: 'Deleted page can be restored in your <a href="/setting">account setting</a> page.',
+            type: "warning",
+            html: true,
+            showCancelButton: true,
+            closeOnConfirm: false,
+            allowOutsideClick: true,
+            showLoaderOnConfirm: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Yes, delete it!",
+            cancelButtonText: "No, cancel please!",
+        },
+        function(){
+            $.ajax({
+                url: that.url,
+                method: 'delete',
+                data: {
+                    _token: csrfToken
+                }
+            }).done(function () {
+                swal("Deleted");
+                setTimeout(function () {
+                    if (that.redirect) {
+                        window.location = that.redirect;
+                    }
+                }, 1000);
+            }).fail(function () {
+                swal("Cannot delete this page.");
+            });
+        });
+    },
+});
