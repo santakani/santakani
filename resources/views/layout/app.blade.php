4710906b72aaaa46fe7c86d850e561fe727a6dbf
Guo Yunhe <guoyunhebrave@gmail.com>
2016-06-15 10:24:26 +0300

    Close button to alert.

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -45,8 +45,9 @@
     @stack('scripts')
 
     @if(App::environment('test'))
-        <div id="test-site-warning" class="alert alert-danger"
+        <div id="test-site-warning" class="alert alert-danger alert-dismissible"
             style="position:fixed;left:10px;right:10px;bottom:10px;margin:0;max-width:600px;z-index:100;">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
             This is a test website and may contain unknown problems. All input data
             will be flushed everyday. If you have any questions and suggestions,
             please <a href="mailto:info@santakani.com">contact us</a>.
