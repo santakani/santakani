0d7d7166ef92e4a06db4bb7a1cddea3d3e2f063b
Guo Yunhe <guoyunhebrave@gmail.com>
2016-06-18 10:24:40 +0300

    Home page button links.

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -12,8 +12,8 @@
                 <h1>Designers + Design Lovers</h1>
                 <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
                 <p>
-                    <a class="btn btn-info btn-lg" href="#" role="button">Explore designer list</a>
-                    <a class="btn btn-success btn-lg" href="#" role="button">Create designer profile</a>
+                    <a class="btn btn-info btn-lg" href="/designer" role="button">Explore designer list</a>
+                    <a class="btn btn-success btn-lg" href="/designer/create" role="button">Create designer profile</a>
                 </p>
             </div><!-- .container -->
         </div><!-- .jumbotron -->
@@ -24,8 +24,8 @@
                 <h1>Design Shops, Design Museums, and More</h1>
                 <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
                 <p>
-                    <a class="btn btn-info btn-lg" href="#" role="button">Open design map</a>
-                    <a class="btn btn-success btn-lg" href="#" role="button">Mark a design shop</a>
+                    <a class="btn btn-info btn-lg" href="/place" role="button">Open design map</a>
+                    <a class="btn btn-success btn-lg" href="/place/create" role="button">Mark a design shop</a>
                 </p>
             </div><!-- .container -->
         </div><!-- .jumbotron -->
@@ -36,8 +36,8 @@
                 <h1>The Story of Endless Creativity</h1>
                 <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
                 <p>
-                    <a class="btn btn-info btn-lg" href="#" role="button">Start reading</a>
-                    <a class="btn btn-success btn-lg" href="#" role="button">Write &amp; publish</a>
+                    <a class="btn btn-info btn-lg" href="/story" role="button">Start reading</a>
+                    <a class="btn btn-success btn-lg" href="/story/create" role="button">Write &amp; publish</a>
                 </p>
             </div><!-- .container -->
         </div><!-- .jumbotron -->
