21a4b582e58835b16d93e0b4800dc1813d7922e4
Guo Yunhe <guoyunhebrave@gmail.com>
2016-06-19 19:38:15 +0300

    EmptyToNull middleware.

---
+++ app/Http/Middleware/EmptyToNull.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of santakani.com
+ *
+ * (c) Guo Yunhe <guoyunhebrave@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+/**
+ * EmptyToNull
+ *
+ * Convert empty string inputs in request to null. Only apply to top level value
+ * in input array, child array will not be checked.
+ *
+ * @author Guo Yunhe <guoyunhebrave@gmail.com>
+ * @see https://github.com/santakani/santakani.com/wiki/Middleware
+ */
+class EmptyToNull {
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        foreach ($request->all() as $key => $value) {
+            if (empty($value)) {
+                $request->merge([$key => null]);
+            }
+        }
+
+        return $next($request);
+    }
+}
