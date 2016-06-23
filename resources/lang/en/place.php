c12a9564dcfc8a098b4ea182da5d212a9912ae36
Guo Yunhe <guoyunhebrave@gmail.com>
2016-06-23 23:18:56 +0300

    Improve place page.

---
+++ resources/lang/en/place.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'address' => 'Address',
+    'create_place_page' => 'Create place page',
+    'location' => 'Location',
+    'map' => 'Map',
+    'museum' => 'Museum',
+    'place' => 'Place',
+    'school' => 'School',
+    'shop' => 'Shop',
+    'showroom' => 'Showroom',
+    'studio' => 'Studio',
+];
--- resources/views/page/place/create.blade.php
+++ resources/views/page/place/create.blade.php
@@ -1,82 +1,97 @@
-@extends('layout.app', [
-    'title' => 'Create Place',
+@extends('layout.create', [
+    'title' => trans('place.create_place_page'),
     'body_id' => 'place-create-page',
-    'body_class' => 'place-create-page place-page create-page',
+    'body_classes' => ['place-create-page', 'place-page'],
     'active_nav' => 'place',
 ])
 
-@section('main')
+@section('panel_title', trans('place.create_place_page'))
+
+@section('panel_body')
 <form class="form-horizontal" action="{{ url('place') }}" method="post">
-    <div class="container">
 
-        <h2><center>Create Place</center></h2>
+    {!! csrf_field() !!}
 
-        @if($errors->any())
-            <div class="form-group">
-                <div class="col-sm-offset-2 col-sm-10 col-md-8">
-                    <div class="alert alert-warning" role="alert">{{$errors->first()}}</div>
-                </div>
-            </div>
-        @endif
+    <div class="form-group {{ $errors->has('name') ? 'has-error' : '' }}">
+        <label for="name-input" class="col-sm-3 control-label">{{ trans('common.name') }}</label>
 
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-        <div class="form-group">
-            <label for="input-name" class="col-sm-2 control-label">Name</label>
-            <div class="col-sm-10 col-md-8">
-                <input name="name" value="{{ old('name') }}" type="text"
-                    required maxlength="255" class="form-control" id="input-name">
-            </div>
+        <div class="col-sm-9">
+            <input name="name" value="{{ old('name') }}" type="text"
+                required maxlength="255" class="form-control" id="name-input">
+            @if ($errors->has('name'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('name') }}</strong>
+                </span>
+            @endif
         </div>
+    </div>
 
-        <div class="form-group">
-            <label for="place-type-select" class="col-sm-2 control-label">
-                Type
-            </label>
+    <div class="form-group {{ $errors->has('type') ? 'has-error' : '' }}">
+        <label for="type-select" class="col-sm-3 control-label">Type</label>
 
-            <div class="col-sm-4 col-md-3">
-                @include('component.place-type-select', [
-                    'selected' => old('type'),
-                    'required' => true,
-                ])
-            </div>
+        <div class="col-sm-9">
+            @include('component.place-type-select', [
+                'id' => 'type-select',
+                'selected' => old('type'),
+                'required' => true,
+            ])
+            @if ($errors->has('type'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('type') }}</strong>
+                </span>
+            @endif
         </div>
+    </div>
 
-        <div class="form-group">
-            <label for="input-address" class="col-sm-2 control-label">Address</label>
-            <div class="col-sm-10 col-md-8">
-                <input type="text" name="address" value="{{ old('address') }}"
-                    maxlength="255" class="form-control" id="input-address" required>
-            </div>
+    <div class="form-group {{ $errors->has('address') ? 'has-error' : '' }}">
+        <label for="address-input" class="col-sm-3 control-label">{{ trans('place.address') }}</label>
+
+        <div class="col-sm-9">
+            <input name="address" value="{{ old('address') }}" type="text"
+                required maxlength="255" class="form-control" id="address-input">
+            @if ($errors->has('address'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('address') }}</strong>
+                </span>
+            @endif
         </div>
+    </div>
 
-        <div class="form-group">
-            <label class="col-sm-2 control-label">
-                City
-            </label>
+    <div class="form-group {{ $errors->has('city_id') ? 'has-error' : '' }}">
+        <label for="city-select" class="col-sm-3 control-label">{{ trans('city.city') }}</label>
 
-            <div class="col-sm-4 col-md-3">
-                <select name="city_id" class="city-select form-control">
-                    @if (!empty(old('city_id')))
-                        <?php $city = \App\City::find(old('city_id')); ?>
-                        <option value="{{ $city->id }}" selected="selected">{{ $city->full_name }}</option>
-                    @endif
-                </select>
-            </div>
+        <div class="col-sm-9">
+            <select name="city_id" id="city-select" class="city-select form-control">
+                @if (!empty(old('city_id')))
+                    <?php $city = \App\City::find(old('city_id')); ?>
+                    <option value="{{ $city->id }}" selected="selected">{{ $city->full_name }}</option>
+                @endif
+            </select>
+            @if ($errors->has('city_id'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('city_id') }}</strong>
+                </span>
+            @endif
         </div>
+    </div>
 
-        <div class="form-group">
-            <label for="input-email" class="col-sm-2 control-label">Email</label>
-            <div class="col-sm-10 col-md-8">
-                <input name="email" value="{{ old('email') }}" type="email"
-                    maxlength="255" class="form-control" id="input-email">
-            </div>
+    <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
+        <label for="email-input" class="col-sm-3 control-label">{{ trans('common.email') }}</label>
+
+        <div class="col-sm-9">
+            <input name="email" value="{{ old('email') }}" type="email"
+                maxlength="255" class="form-control" id="email-input">
+            @if ($errors->has('email'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+            @endif
         </div>
+    </div>
 
-        <div class="form-group">
-            <div class="col-sm-offset-2 col-sm-10">
-                <button type="submit" class="btn btn-default">Create</button>
-            </div>
+    <div class="form-group">
+        <div class="col-sm-offset-3 col-sm-9">
+            <button type="submit" class="btn btn-primary">{{ trans('common.create') }}</button>
         </div>
     </div>
 </form>
