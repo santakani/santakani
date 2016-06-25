8f746c877866eaddf95e3889ee3fb36d42d16e79
Guo Yunhe <guoyunhebrave@gmail.com>
2016-06-25 17:54:06 +0300

    Define user authorization.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,38 +28,143 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies($gate);
 
-        $gate->define('edit-page', function ($user, $page_model) {
-            if ($user->hasRole('admin') || $user->hasRole('editor')) {
-                return true;
-            }
+        //======================================================================
+        // Authorization Based On Action
+        //======================================================================
 
-            return $user->id === $page_model->user_id;
+        /*
+         * 1. Action names use kebab-case.
+         * 2. Separate actions as detailed as possible. For example, "edit-tag"
+         *    and "create-tag" are different actions.
+         * 3. Always use whitelist logic.
+         */
+
+        // Legacy
+        $gate->define('edit-page', function ($user, $page) {
+            return $user->role === 'admin' || $user->role === 'editor' || $user->id === $page->user_id;
         });
 
-        $gate->define('translate-page', function ($user, $page_model) {
-            if ($user->hasRole('admin') || $user->hasRole('editor') || $user->hasRole('translator')) {
-                return true;
-            }
 
-            return $user->id === $page_model->user_id;
+
+        $gate->define('set-user-role', function ($user, $target_user) {
+            // Admin cannot set roles of other admins
+            return $user->role === 'admin' && $target_user->role !== 'admin';
         });
 
-        // Action: create tag
-        // Roles: admin, editor
+        $gate->define('unset-user-role', function ($user, $target_user) {
+            if ($user->id === $target_user->id) {
+                return true; // User can unset own role
+            } elseif ($user->role === 'admin' && $target_user->role !== 'admin') {
+                return true; // Admin can unset roles of users but not other admins
+            } else {
+                return false;
+            }
+        });
+
+
+
+        $gate->define('edit-designer', function ($user, $designer) {
+            return $user->role === 'admin' || $user->role === 'editor' || $user->id === $designer->user_id;
+        });
+
+        $gate->define('delete-designer', function ($user, $designer) {
+            return $user->role === 'admin' || $user->role === 'editor' || $user->id === $designer->user_id;
+        });
+
+        $gate->define('force-delete-designer', function ($user, $designer) {
+            return $user->role === 'admin' || $user->role === 'editor';
+        });
+
+
+
+        $gate->define('edit-place', function ($user, $place) {
+            return $user->role === 'admin' || $user->role === 'editor' || $user->id === $place->user_id;
+        });
+
+        $gate->define('delete-place', function ($user, $place) {
+            return $user->role === 'admin' || $user->role === 'editor' || $user->id === $place->user_id;
+        });
+
+        $gate->define('force-delete-place', function ($user, $place) {
+            return $user->role === 'admin' || $user->role === 'editor';
+        });
+
+
+
+        $gate->define('edit-story', function ($user, $story) {
+            return $user->role === 'admin' || $user->role === 'editor' || $user->id === $story->user_id;
+        });
+
+        $gate->define('delete-story', function ($user, $story) {
+            return $user->role === 'admin' || $user->role === 'editor' || $user->id === $story->user_id;
+        });
+
+        $gate->define('force-delete-story', function ($user, $story) {
+            return $user->role === 'admin' || $user->role === 'editor';
+        });
+
+
+
+        $gate->define('delete-image', function ($user, $image) {
+            return $user->role === 'admin' || $user->role === 'editor' || $user->id === $image->user_id;
+        });
+
+        $gate->define('force-delete-image', function ($user, $image) {
+            return $user->role === 'admin' || $user->role === 'editor';
+        });
+
+
+
+        $gate->define('create-city', function ($user) {
+            return $user->role === 'admin' || $user->role === 'editor';
+        });
+
+        $gate->define('edit-city', function ($user) {
+            return $user->role === 'admin' || $user->role === 'editor';
+        });
+
+        $gate->define('delete-city', function ($user) {
+            return $user->role === 'admin' || $user->role === 'editor';
+        });
+
+        $gate->define('force-delete-city', function ($user) {
+            return $user->role === 'admin' || $user->role === 'editor';
+        });
+
+
+
+        $gate->define('create-country', function ($user) {
+            return $user->role === 'admin' || $user->role === 'editor';
+        });
+
+        $gate->define('edit-country', function ($user) {
+            return $user->role === 'admin' || $user->role === 'editor';
+        });
+
+        $gate->define('delete-country', function ($user) {
+            return $user->role === 'admin' || $user->role === 'editor';
+        });
+
+        $gate->define('force-delete-country', function ($user) {
+            return $user->role === 'admin' || $user->role === 'editor';
+        });
+
+
+
         $gate->define('create-tag', function ($user) {
-            return $user->hasRole('admin') || $user->hasRole('editor');
+            return $user->role === 'admin' || $user->role === 'editor';
         });
 
-        // Action: edit tag
-        // Roles: admin, editor
         $gate->define('edit-tag', function ($user) {
-            return $user->hasRole('admin') || $user->hasRole('editor');
+            return $user->role === 'admin' || $user->role === 'editor';
         });
 
-        // Action: delete tag
-        // Roles: admin, editor
         $gate->define('delete-tag', function ($user) {
-            return $user->hasRole('admin') || $user->hasRole('editor');
+            return $user->role === 'admin' || $user->role === 'editor';
+        });
+
+        $gate->define('force-delete-tag', function ($user) {
+            return $user->role === 'admin' || $user->role === 'editor';
         });
     }
 }
