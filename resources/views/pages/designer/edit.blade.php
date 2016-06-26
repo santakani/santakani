5cfdda8fd6827286e60dcf38a7cbd73f7f1856d8
Guo Yunhe <guoyunhebrave@gmail.com>
2016-06-27 00:53:45 +0300

    Designer edit page support translation

--- resources/views/pages/designer/edit.blade.php
+++ resources/views/pages/designer/edit.blade.php
@@ -1,40 +1,74 @@
 @extends('layouts.app', [
-    'title' => 'Edit: ' . $designer->name,
+    'title' => trans('common.edit') . ': ' . $designer->text('name') . ' - ' . trans('designer.designer'),
     'body_id' => 'designer-edit-page',
     'body_classes' => ['designer-edit-page', 'edit-page', 'designer-page'],
     'active_nav' => 'designer',
 ])
 
+@section('header')
+    <div class="container">
+        <h1 class="page-header">{{ trans('common.edit') }}: {{ $designer->text('name') }}</h1>
+    </div>
+@endsection
+
 @section('main')
-<div class="container">
-    <form id="designer-edit-form" class="form-horizontal" action="{{ $designer->url }}"
-        data-id="{{ $designer->id }}">
+    <div class="container">
+        <form id="designer-edit-form" class="form" action="{{ $designer->url }}"
+            data-id="{{ $designer->id }}">
 
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-        <div class="form-group">
-            <div class="col-sm-offset-2 col-sm-10 col-md-8">
-                <div class="alert alert-warning" style="display:none;" role="alert"></div>
-            </div>
-        </div>
+            <div class="tab-pane-group">
+                <!-- Nav tabs -->
+                <ul id="translation-tabs" class="nav nav-tabs">
+                    @foreach (App\Localization\Languages::getLanguageList() as $locale => $names)
+                        <li class="{{ $locale==='en'?'active':'' }}">
+                            <a href="#translation-{{ $locale }}" data-toggle="tab" title="{{ $names['native'] }}">
+                                {{ $names['localized'] }}
+                            </a>
+                        </li>
+                    @endforeach
+                    <li class="more dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                            More <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-right"></ul>
+                    </li>
+                </ul>
 
-        <div class="form-group">
-            <label for="input-name" class="col-sm-2 control-label">
-                Name
-            </label>
+                <!-- Tab panes -->
+                <div class="tab-content">
+                    @foreach (App\Localization\Languages::getLanguageCodeList() as $locale)
+                        <?php $translation = $designer->translations()->where('locale', $locale)->first(); ?>
+                        <div id="translation-{{ $locale }}" class="tab-pane {{ $locale==='en'?'active':'' }}">
+                            <div class="form-group">
+                                <label class="control-label">Name</label>
+                                <input name="translations[{{ $locale }}][name]"
+                                    value="{{ $translation->name or '' }}"
+                                    class="form-control" type="text" maxlength="255">
+                            </div>
 
-            <div class="col-sm-10 col-md-8">
-                <input name="name" type="text" class="form-control" id="input-name"
-                    value="{{ $designer->text('name', 'en') }}">
-            </div>
-        </div>
+                            <div class="form-group">
+                                <label class="control-label">Tagline</label>
+                                <input name="translations[{{ $locale }}][tagline]"
+                                    value="{{ $translation->tagline or '' }}"
+                                    class="form-control" type="text" maxlength="255">
+                            </div>
 
-        <div id="image-form-group" class="form-group">
-            <label class="col-sm-2 control-label">
-                Cover image
-            </label>
+                            <div class="form-group">
+                                <label class="control-label">Content</label>
+                                <textarea name="translations[{{ $locale }}][content]"
+                                    class="content-editor">{{ $translation->content or '' }}</textarea>
+                            </div>
+                        </div>
+                    @endforeach
+                </div><!-- /.tab-content -->
+            </div><!-- /.tab-pane -->
 
-            <div class="col-sm-10 col-md-8">
+            <br/>
+
+            <div id="image-form-group" class="form-group">
+                <label class="control-label">Cover image</label>
                 <div id="cover-editor" class="cover-editor">
                     <p><button type="button" class="btn btn-default"><i class="fa fa-picture-o"></i> Choose</button></p>
                     @if ($designer->image_id)
@@ -44,37 +78,9 @@
                     @endif
                 </div>
             </div>
-        </div>
 
-        <div class="form-group">
-            <label for="input-tagline" class="col-sm-2 control-label">
-                Tagline
-            </label>
-
-            <div class="col-sm-10 col-md-8">
-                <input type="text" name="tagline" class="form-control" id="input-tagline"
-                    placeholder="Express design philosophy in short" maxlength="255"
-                    value="{{ $designer->text('tagline', 'en') }}">
-            </div>
-        </div>
-
-        <div class="form-group">
-            <label for="input-content" class="col-sm-2 control-label">
-                Content
-            </label>
-
-            <div class="col-sm-10 col-md-8">
-                <textarea name="content" class="form-control tinymce"
-                    id="input-content">{{ $designer->text('content', 'en') }}</textarea>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <label class="col-sm-2 control-label">
-                Gallery
-            </label>
-
-            <div class="col-sm-10 col-md-8">
+            <div class="form-group">
+                <label class="control-label">Gallery</label>
                 <div id="gallery-editor" class="gallery-editor">
                     <p><button type="button" class="btn btn-default"><i class="fa fa-picture-o"></i> Choose</button></p>
                     <div class="images clearfix">
@@ -84,90 +90,65 @@
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="form-group">
-            <label class="col-sm-2 control-label">
-                City
-            </label>
-
-            <div class="col-sm-4 col-md-3">
+            <div class="form-group">
+                <label class="control-label">City</label>
                 <select name="city_id" id="city-select" class="city-select form-control">
                     @if (!empty($designer->city_id))
                         <option value="{{ $designer->city_id }}" selected="selected">{{ $designer->city->full_name }}</option>
                     @endif
                 </select>
             </div>
-        </div>
 
-        <div class="form-group">
-            <label class="col-sm-2 control-label">Tags</label>
-
-            <div class="col-sm-10 col-md-8">
+            <div class="form-group">
+                <label class="control-label">Tags</label>
                 <select name="tag_ids[]" class="tag-select" style="width: 100%" multiple="multiple">
                     @foreach ($designer->tags as $tag)
                         <option value="{{ $tag->id }}" selected="selected">{{ $tag->text('name') }}</option>
                     @endforeach
                 </select>
             </div>
-        </div>
 
-        <div class="form-group">
-            <label for="input-email" class="col-sm-2 control-label">Email</label>
-            <div class="col-sm-10 col-md-8">
+            <div class="form-group">
+                <label class="control-label">Email</label>
                 <input name="email" value="{{ $designer->email }}" type="email"
-                    maxlength="255" class="form-control" id="input-email">
+                    maxlength="255" class="form-control">
             </div>
-        </div>
 
-        <div class="form-group">
-            <label for="input-website" class="col-sm-2 control-label">Website</label>
-            <div class="col-sm-10 col-md-8">
+            <div class="form-group">
+                <label class="control-label">Website</label>
                 <input name="website" value="{{ $designer->website }}" type="url"
-                    maxlength="255" class="form-control" id="input-website">
+                    maxlength="255" class="form-control">
             </div>
-        </div>
 
-        <div class="form-group">
-            <label for="input-facebook" class="col-sm-2 control-label">Facebook</label>
-            <div class="col-sm-10 col-md-8">
+            <div class="form-group">
+                <label class="control-label">Facebook</label>
                 <input name="facebook" value="{{ $designer->facebook }}" type="url"
-                    maxlength="255" class="form-control" id="input-facebook">
+                    maxlength="255" class="form-control">
             </div>
-        </div>
 
-        <div class="form-group">
-            <label for="input-twitter" class="col-sm-2 control-label">Twitter</label>
-            <div class="col-sm-10 col-md-8">
+            <div class="form-group">
+                <label class="control-label">Twitter</label>
                 <input name="twitter" value="{{ $designer->twitter }}" type="url"
-                    maxlength="255" class="form-control" id="input-twitter">
+                    maxlength="255" class="form-control">
             </div>
-        </div>
 
-        <div class="form-group">
-            <label for="input-google-plus" class="col-sm-2 control-label">Google+</label>
-            <div class="col-sm-10 col-md-8">
+            <div class="form-group">
+                <label class="control-label">Google+</label>
                 <input name="google_plus" value="{{ $designer->google_plus }}" type="url"
-                    maxlength="255" class="form-control" id="input-google-plus">
+                    maxlength="255" class="form-control">
             </div>
-        </div>
 
-        <div class="form-group">
-            <label for="input-instagram" class="col-sm-2 control-label">Instagram</label>
-            <div class="col-sm-10 col-md-8">
+            <div class="form-group">
+                <label class="control-label">Instagram</label>
                 <input name="instagram" value="{{ $designer->instagram }}" type="url"
-                    maxlength="255" class="form-control" id="input-instagram">
+                    maxlength="255" class="form-control">
             </div>
-        </div>
 
-        <div class="form-group">
-            <div class="col-sm-offset-2 col-sm-10">
-                <button type="submit" class="btn btn-default">Save</button>
-            </div>
-        </div>
+            <button type="submit" class="btn btn-default">Save</button>
 
-    </form>
-</div><!-- .container -->
+        </form>
+    </div><!-- .container -->
 @endsection
 
 @push('templates')
