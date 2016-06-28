f23959302a9fc1e8db2577a8f6474e491d8588d3
Guo Yunhe <guoyunhebrave@gmail.com>
2016-06-28 23:02:33 +0300

    Place edit page support translations

--- resources/assets/js/pages/place/edit.js
+++ resources/assets/js/pages/place/edit.js
@@ -6,8 +6,12 @@
  * Script - assets/js/edit/ajax/designer-edit.js
  */
 
-var ImagePreview = require('../../views/image-preview');
+require('../../components/forms/edit-form');
+
 var Image = require('../../models/image');
+
+var ImageChooser = require('../../views/image-upload/image-chooser');
+var ImagePreview = require('../../views/image-preview');
 var ImageManager = require('../../views/image-manager');
 var ContentEditor = require('../../views/content-editor');
 var GalleryEditor = require('../../views/gallery-editor');
@@ -15,99 +19,64 @@ var CitySelect = require('../../views/city-select');
 var TagSelect = require('../../views/tag-select');
 var CoordinateSelect = require('../../views/coordinate-select');
 
-$(function () {
 
-    // Page check
-    if ($('#place-edit-page').length === 0) {
-        return;
-    }
-
-    // Image manager
-    var manager = new ImageManager({
-        parentType: 'place',
-        parentId: parseInt($('form').data('id'))
-    });
-
-    // Cover
-    var coverPreview = new ImagePreview({
-        el: '#image-form-group .image-preview',
-        width: 600,
-        height: 200,
-        size: 'medium',
-        inputName: 'image_id',
-    });
-
-    $('#image-form-group button').click(function () {
-        manager.call({
-            multiple: false,
-            done: function (image) {
-                coverPreview.model.set(image.attributes);
-            }
-        });
-    });
-
-    var contentEditor = new ContentEditor({el: '#input-content', imageManager: manager});
-
-    var galleryEditor = new GalleryEditor({el: '#gallery-editor', imageManager: manager});
-
-    var citySelect = new CitySelect({el: '.city-select'});
-
-    var coordinateSelect = new CoordinateSelect();
-
-
-
-    //====================================================================
-    // Update coordinate when changing address
-    //====================================================================
-
-    var searchTimeout;
-
-    function searchCoordinateTimer () {
-        clearTimeout(searchTimeout);
-        searchTimeout = setTimeout(searchCoordinate, 500);
-    }
-
-    function searchCoordinate() {
-        var address = $('#address-input').val().trim();
-        var city = $('#city-select option').text().trim();
-        if (address.length > 0 && city.length > 0) {
-            var query = address + ', ' + city;
-            coordinateSelect.search(query);
-        }
-    }
-
-    if (!coordinateSelect.latitude || !coordinateSelect.longitude) {
-        searchCoordinate();
-    }
-
-    $('#address-input')[0].oninput = searchCoordinateTimer;
-    $('#city-select')[0].onchange = searchCoordinate;
-    $('#search-coordinate-button')[0].onclick = searchCoordinate;
-
-
-    var tagSelect = new TagSelect({el: '.tag-select'});
-
-    // Submit form
-    $('button[type="submit"]').click(function (e) {
-        e.preventDefault();
-
-        $.ajax({
-            method: 'PUT',
-            url: $('#place-edit-form').attr('action'),
-            data: $('#place-edit-form').serializeArray()
-        }).done(function () {
-            window.location.href = $('#place-edit-form').attr('action');
-        }).fail(function (error) {
-            var response = error.responseJSON;
-            var $alert = $('#place-edit-form .alert');
-            var message = '';
-
-            for (var id in response) {
-                message += '<p>' + response[id] + '</p>';
-            }
-
-            $alert.html(message).show().scrollTo();
-        });
-    });
-
+// Image manager
+var manager = new ImageManager({
+    parentType: 'place',
+    parentId: parseInt($('.edit-form').data('id'))
 });
+
+
+// Content editor
+$('.content-editor').each(function () {
+    new ContentEditor({el: this, imageManager: manager});
+});
+
+
+// Cover chooser
+var coverChooser = new ImageChooser({
+    el: '#cover-chooser',
+    width: 600,
+    height: 200,
+    inputName: 'image_id',
+    manager: manager,
+});
+
+
+// Gallery editor
+var galleryEditor = new GalleryEditor({el: '#gallery-editor', imageManager: manager});
+
+
+// City select
+var citySelect = new CitySelect({el: '.city-select'});
+// Coordinate select
+var coordinateSelect = new CoordinateSelect();
+
+// Update coordinate when changing address
+var searchTimeout;
+
+if (!coordinateSelect.latitude || !coordinateSelect.longitude) {
+    searchCoordinate();
+}
+
+$('#address-input')[0].oninput = searchCoordinateTimer;
+$('#city-select')[0].onchange = searchCoordinate;
+$('#search-coordinate-button')[0].onclick = searchCoordinate;
+
+function searchCoordinateTimer () {
+    clearTimeout(searchTimeout);
+    searchTimeout = setTimeout(searchCoordinate, 500);
+}
+
+function searchCoordinate() {
+    var address = $('#address-input').val().trim();
+    var city = $('#city-select option').text().trim();
+    if (address.length > 0 && city.length > 0) {
+        var query = address + ', ' + city;
+        coordinateSelect.search(query);
+    }
+}
+
+
+// Tag select
+var tagSelect = new TagSelect({el: '.tag-select'});
--- resources/views/pages/place/edit.blade.php
+++ resources/views/pages/place/edit.blade.php
@@ -1,83 +1,78 @@
 @extends('layouts.app', [
-    'title' => 'Edit: ' . $place->name,
+    'title' => trans('common.edit').': '.$place->text('name').' - '.trans('place.place'),
     'body_id' => 'place-edit-page',
     'body_classes' => ['place-edit-page', 'place-page', 'edit-page'],
     'active_nav' => 'place',
 ])
 
 @section('main')
-<div class="container">
-    <form id="place-edit-form" class="form-horizontal" action="{{ $place->url }}"
-        data-id="{{ $place->id }}">
 
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    <form class="edit-form" action="{{ $place->url }}" data-id="{{ $place->id }}">
+        <div class="container">
 
-        <div class="form-group">
-            <div class="col-sm-offset-2 col-sm-10 col-md-8">
-                <div class="alert alert-warning" style="display:none;" role="alert"></div>
-            </div>
-        </div>
+            {!! csrf_field() !!}
 
-        <div class="form-group">
-            <label for="input-name" class="col-sm-2 control-label">
-                Name
-            </label>
+            <div class="tab-pane-group">
+                <!-- Nav tabs -->
+                <ul id="translation-tabs" class="nav nav-tabs">
+                    @foreach (App\Localization\Languages::getLanguageList() as $locale => $names)
+                        <li class="{{ $locale==='en'?'active':'' }}">
+                            <a href="#translation-{{ $locale }}" data-toggle="tab" title="{{ $names['native'] }}">
+                                {{ $names['localized'] }}
+                            </a>
+                        </li>
+                    @endforeach
+                    <li class="more dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                            More <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-right"></ul>
+                    </li>
+                </ul>
 
-            <div class="col-sm-10 col-md-8">
-                <input id="input-name" class="form-control" type="text"
-                    name="name" value="{{ $place->text('name', 'en') }}"
-                    placeholder="Name of place" maxlength="255">
-            </div>
-        </div>
+                <!-- Tab panes -->
+                <div class="tab-content">
+                    @foreach (App\Localization\Languages::getLanguageCodeList() as $locale)
+                        <?php $translation = $place->translations()->where('locale', $locale)->first(); ?>
+                        <div id="translation-{{ $locale }}" class="tab-pane {{ $locale==='en'?'active':'' }}">
+                            <div class="form-group">
+                                <label class="control-label">Name</label>
+                                <input name="translations[{{ $locale }}][name]"
+                                    value="{{ $translation->name or '' }}"
+                                    class="form-control" type="text" maxlength="255">
+                            </div>
 
-        <div class="form-group">
-            <label for="place-type-select" class="col-sm-2 control-label">
-                Type
-            </label>
+                            <div class="form-group">
+                                <label class="control-label">Content</label>
+                                <textarea name="translations[{{ $locale }}][content]"
+                                    class="content-editor">{{ $translation->content or '' }}</textarea>
+                            </div>
+                        </div>
+                    @endforeach
+                </div><!-- /.tab-content -->
+            </div><!-- /.tab-pane -->
 
-            <div class="col-sm-4 col-md-3">
+            <br/>
+
+            <div class="form-group">
+                <label class="control-label">Type</label>
                 @include('components.place-type-select', [
                     'selected' => $place->type,
                     'required' => true,
                 ])
             </div>
-        </div>
 
-        <div id="image-form-group" class="form-group">
-            <label class="col-sm-2 control-label">
-                Cover image
-            </label>
-
-            <div class="col-sm-10 col-md-8">
-                <div id="cover-editor" class="cover-editor">
-                    <p><button type="button" class="btn btn-default"><i class="fa fa-picture-o"></i> Choose</button></p>
-                    @if ($place->image_id)
-                        @include('components.image-preview', ['image' => $place->image])
-                    @else
-                        @include('components.image-preview')
-                    @endif
-                </div>
+            <div class="form-group">
+                <label class="control-label">Cover image</label>
+                <div id="cover-chooser" class="image-chooser"
+                    data-id="{{ $place->image_id }}"
+                    data-mime="{{ $place->image->mime_type or '' }}"
+                    data-width="{{ $place->image->width or '' }}"
+                    data-height="{{ $place->image->height or '' }}"></div>
             </div>
-        </div>
 
-        <div class="form-group">
-            <label for="input-content" class="col-sm-2 control-label">
-                Content
-            </label>
-
-            <div class="col-sm-10 col-md-8">
-                <textarea id="input-content" name="content">{{
-                    old('content')?old('content'):$place->text('content', 'en')
-                }}</textarea>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <label class="col-sm-2 control-label">
-                Gallery
-            </label>
-
-            <div class="col-sm-10 col-md-8">
+            <div class="form-group">
+                <label class="control-label">Gallery</label>
                 <div id="gallery-editor" class="gallery-editor">
                     <p><button type="button" class="btn btn-default"><i class="fa fa-picture-o"></i> Choose</button></p>
                     <div class="images clearfix">
@@ -87,14 +82,9 @@
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="form-group">
-            <label class="col-sm-2 control-label">
-                City
-            </label>
-
-            <div class="col-sm-4 col-md-3">
+            <div class="form-group">
+                <label class="control-label">City</label>
                 <select name="city_id" id="city-select" class="city-select form-control">
                     @if (!empty($place->city_id))
                         <option value="{{ $place->city_id }}" selected="selected">
@@ -103,40 +93,25 @@
                     @endif
                 </select>
             </div>
-        </div>
 
-        <div class="form-group">
-            <label for="address-input" class="col-sm-2 control-label">
-                Address
-            </label>
-
-            <div class="col-sm-8 col-md-6">
-                <input id="address-input" class="form-control" type="text"
-                    name="address" value="{{ $place->address }}" maxlength="255">
-            </div>
-            <div class="col-sm-2">
-                <button id="search-coordinate-button" class="btn btn-info btn-block" type="button">
+            <div class="form-group">
+                <label class="control-label">Address</label>
+                <input name="address" value="{{ $place->address }}" type="text"
+                    maxlength="255" class="form-control" id="address-input">
+                <button id="search-coordinate-button" class="btn btn-info" type="button">
                     <i class="fa fa-map-marker fa-lg"></i> Mark On Map
                 </button>
             </div>
-        </div>
 
-        <div class="form-group">
-            <label class="col-sm-2 control-label">
-                Coordinates
-            </label>
-            <div class="col-sm-10 col-md-8">
+            <div class="form-group">
                 @include('components.coordinate-select', [
                     'latitude' => $place->latitude,
                     'longitude' => $place->longitude,
                 ])
             </div>
-        </div>
 
-        <div class="form-group">
-            <label class="col-sm-2 control-label">Tags</label>
-
-            <div class="col-sm-10 col-md-8">
+            <div class="form-group">
+                <label class="control-label">Tags</label>
                 <select name="tag_ids[]" class="tag-select" style="width: 100%" multiple="multiple">
                     @foreach ($place->tags as $tag)
                         <option value="{{ $tag->id }}" selected="selected">
@@ -145,58 +120,41 @@
                     @endforeach
                 </select>
             </div>
-        </div>
 
-        <div class="form-group">
-            <label for="phone-input" class="col-sm-2 control-label">
-                Phone
-            </label>
-            <div class="col-sm-10 col-md-8">
+            <div class="form-group">
+                <label class="control-label">Phone</label>
                 <input name="phone" value="{{ $place->phone }}" type="tel"
-                       id="phone-input" class="form-control" maxlength="255">
+                    id="phone-input" class="form-control" maxlength="255">
             </div>
-        </div>
 
-        <div class="form-group">
-            <label for="input-email" class="col-sm-2 control-label">Email</label>
-            <div class="col-sm-10 col-md-8">
+            <div class="form-group">
+                <label class="control-label">Email</label>
                 <input name="email" value="{{ $place->email }}" type="email"
                     maxlength="255" class="form-control" id="input-email">
             </div>
-        </div>
 
-        <div class="form-group">
-            <label for="input-website" class="col-sm-2 control-label">Website</label>
-            <div class="col-sm-10 col-md-8">
+            <div class="form-group">
+                <label class="control-label">Website</label>
                 <input name="website" value="{{ $place->website }}" type="url"
                     maxlength="255" class="form-control" id="input-website">
             </div>
-        </div>
 
-        <div class="form-group">
-            <label for="input-facebook" class="col-sm-2 control-label">Facebook</label>
-            <div class="col-sm-10 col-md-8">
+            <div class="form-group">
+                <label class="control-label">Facebook</label>
                 <input name="facebook" value="{{ $place->facebook }}" type="url"
                     maxlength="255" class="form-control" id="input-facebook">
             </div>
-        </div>
 
-        <div class="form-group">
-            <label for="input-google-plus" class="col-sm-2 control-label">Google+</label>
-            <div class="col-sm-10 col-md-8">
+            <div class="form-group">
+                <label class="control-label">Google+</label>
                 <input name="google_plus" value="{{ $place->google_plus }}" type="url"
                     maxlength="255" class="form-control" id="input-google-plus">
             </div>
-        </div>
 
-        <div class="form-group">
-            <div class="col-sm-offset-2 col-sm-10">
-                <button type="submit" class="btn btn-default">Save</button>
-            </div>
-        </div>
+            <button type="submit" class="btn btn-primary">Save</button>
 
+        </div><!-- /.container -->
     </form>
-</div><!-- .container -->
 @endsection
 
 @push('templates')
