c04540c6aee803f260e9508d53cecdb5e5c8627d
Guo Yunhe <guoyunhebrave@gmail.com>
2016-06-30 14:59:45 +0300

    Country flag library

--- gulpfile.js
+++ gulpfile.js
@@ -22,6 +22,7 @@ elixir(function(mix) {
     mix.copy('node_modules/font-awesome/fonts', 'public/fonts/font-awesome');
     mix.copy('node_modules/lightgallery/dist/fonts', 'public/fonts/lightgallery');
     mix.copy('node_modules/lightgallery/dist/img', 'public/img/lightgallery');
+    mix.copy('node_modules/flag-icon-css/flags', 'public/img/flags');
     mix.copy('node_modules/roboto-fontface/fonts', 'public/fonts/roboto');
     mix.copy('node_modules/tinymce', 'public/lib/tinymce');
 
