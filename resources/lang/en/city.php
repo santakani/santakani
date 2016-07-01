c5ec856e1b5b20a96855e9732d37862aa267f2c1
Guo Yunhe <guoyunhebrave@gmail.com>
2016-07-01 15:10:55 +0300

    Rename city.php to geo.php

--- resources/lang/en/city.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return array(
-    'city' => 'City',
-    'country' => 'Country',
-);
---
+++ resources/lang/en/geo.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+    'cities' => 'Cities',
+    'city' => 'City',
+    'continent' => 'Continent',
+    'countries' => 'Countries',
+    'country' => 'Country',
+    'country_code' => 'Country code',
+    'currency' => 'Currency',
+    'latitude' => 'Latitude',
+    'longitude' => 'Longitude',
+    'timezone' => 'Timezone',
+);
