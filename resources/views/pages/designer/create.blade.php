66a637eba8ed84b5188acb3464b198a179042dee
Guo Yunhe <guoyunhebrave@gmail.com>
2016-07-07 17:03:57 +0300

    Mark optional and public email field

--- resources/views/pages/designer/create.blade.php
+++ resources/views/pages/designer/create.blade.php
@@ -8,48 +8,47 @@
 @section('panel_title', trans('designer.create_a_designer_page'))
 
 @section('panel_body')
-<form class="form" action="{{ url('designer') }}" method="post">
+    <form class="form" action="{{ url('designer') }}" method="post">
 
-    {!! csrf_field() !!}
+        {!! csrf_field() !!}
 
-    <div class="form-group {{ $errors->has('name') ? 'has-error' : '' }}">
-        <label class="control-label">{{ trans('common.name') }}</label>
-        <input name="name" value="{{ old('name') }}" type="text" required
-               maxlength="255" class="form-control">
-        @if ($errors->has('name'))
-            <span class="help-block">
-                <strong>{{ $errors->first('name') }}</strong>
-            </span>
-        @endif
-    </div>
-
-    <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
-        <label class="control-label">{{ trans('common.email_address') }}</label>
-
-        <input name="email" value="{{ old('email') }}" type="email"
-            maxlength="255" class="form-control">
-        @if ($errors->has('email'))
-            <span class="help-block">
-                <strong>{{ $errors->first('email') }}</strong>
-            </span>
-        @endif
-    </div>
-
-    <div class="form-group {{ $errors->has('city_id') ? 'has-error' : '' }}">
-        <label class="control-label">{{ trans('geo.city') }}</label>
-        <select name="city_id" id="city-select" class="city-select form-control">
-            @if (!empty(old('city_id')))
-                <?php $city = \App\City::find(old('city_id')); ?>
-                <option value="{{ $city->id }}" selected="selected">{{ $city->full_name }}</option>
+        <div class="form-group {{ $errors->has('name') ? 'has-error' : '' }}">
+            <label class="control-label">{{ trans('common.name') }}</label>
+            <input name="name" value="{{ old('name') }}" type="text" required
+                   maxlength="255" class="form-control">
+            @if ($errors->has('name'))
+                <span class="help-block">{{ $errors->first('name') }}</span>
             @endif
-        </select>
-        @if ($errors->has('city_id'))
-            <span class="help-block">{{ $errors->first('city_id') }}</span>
-        @endif
-    </div>
+        </div>
 
-    <div class="form-group">
-        <button type="submit" class="btn btn-primary">{{ trans('common.create') }}</button>
-    </div>
-</form>
+        <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
+            <label class="control-label">
+                {{ trans('common.email_address') }}
+                <span class="label label-info">{{ trans('common.optional') }}</span>
+                <span class="label label-warning">{{ trans('common.public') }}</span>
+            </label>
+            <input name="email" value="{{ old('email') }}" type="email"
+                   maxlength="255" class="form-control">
+            @if ($errors->has('email'))
+                <span class="help-block">{{ $errors->first('email') }}</span>
+            @endif
+        </div>
+
+        <div class="form-group {{ $errors->has('city_id') ? 'has-error' : '' }}">
+            <label class="control-label">{{ trans('geo.city') }}</label>
+            <select name="city_id" id="city-select" class="city-select form-control">
+                @if (!empty(old('city_id')))
+                    <?php $city = \App\City::find(old('city_id')); ?>
+                    <option value="{{ $city->id }}" selected="selected">{{ $city->full_name }}</option>
+                @endif
+            </select>
+            @if ($errors->has('city_id'))
+                <span class="help-block">{{ $errors->first('city_id') }}</span>
+            @endif
+        </div>
+
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary">{{ trans('common.create') }}</button>
+        </div>
+    </form>
 @endsection
