799ddf28894dbeea2b0ecaa42f55ec10218648d7
Guo Yunhe <guoyunhebrave@gmail.com>
2016-07-12 21:59:40 +0300

    Terms page

---
+++ resources/views/pages/terms.blade.php
@@ -0,0 +1,148 @@
+@extends('layouts.app', [
+    'title' => trans('common.terms_of_service'),
+    'body_id' => 'terms-page',
+    'body_classes' => ['terms-page', 'site-page'],
+])
+
+@section('main')
+    <div class="container">
+        <h1 class="page-header">{{ trans('common.terms_of_service') }}</h1>
+
+        <p>This Statement of Rights and Responsibilities ("Statement," "Terms," or "SRR") derives from the Santakani Principles, and is our terms of service that governs our relationship with users and others who interact with Santakani, as well as Santakani brands, products and services, which we call the “Santakani Services” or “Services”. By using or accessing the Santakani Services, you agree to this Statement, as updated from time to time.</p>
+
+        <h2>1. Basic term</h2>
+
+        <p>You are responsible for your use of the Services, for any Content you post to the Services, and for any consequences thereof. Most Content you submit, post, or display through the Santakani Services is public by default and will be able to be viewed by other users and through third party services and websites.</p>
+
+        <h2>2. Privacy</h2>
+        <p>Your privacy is very important to us. We designed our Privacy Policy to make important disclosures about how you can use Santakani to share with others and how we collect and can use your content and information. We encourage you to read the Privacy, and to use it to help you make informed decisions.</p>
+
+        <h2>3. Passwords</h2>
+
+        <p>You are responsible for safeguarding the password that you use to access the Services and for any activities or actions under your password. We encourage you to use “strong” passwords (passwords that use a combination of upper and lower case letters, numbers and symbols) with your account. Santakni cannot and will not be liable for any loss or damage arising from your failure to comply with the above.</p>
+
+        <h2>4. Content on the Services</h2>
+
+        <p>Content that you post using our Services is your content (so let’s refer to it as “Your Content”). We don’t make any claim to it. That includes anything you post using our Services (like usernames, shop names, profile pictures, listing photos, listing descriptions, reviews, comments, videos, etc.).</p>
+
+        <h3>4.1. Responsibility for Your Content</h3>
+
+        <p>You are solely responsible for Your Content. You represent that you have all necessary rights to Your Content and that you’re not infringing or violating any third party’s rights by posting it.</p>
+
+        <h3>4.2. Rights You Grant Santakani</h3>
+
+        <p>By posting Your Content, you grant Santakani a non-exclusive, worldwide, royalty-free, irrevocable, sub-licensable, perpetual license to use, display, edit, modify, reproduce, distribute, store, and prepare derivative works of Your Content to provide the Services and to promote Santakani or the Services in general, in any formats and through any channels, including across any Santakani Services or third-party websites or advertising medium.</p>
+
+        <h3>4.3. Reporting Unauthorized Content</h3>
+
+        <p>Santakani has great respect for intellectual property rights, and is committed to following appropriate legal procedures to remove infringing content from the Services. If content that you own or have rights to has been posted to the Services without your permission and you want it removed,If Your Content infringes another person’s intellectual property, we will remove it if we receive proper notice. We’ll notify you if that happens.</p>
+
+        <h3>4.4. Inappropriate, False or Misleading Content</h3>
+
+        <p>You agree that you will not post any content that is abusive, threatening, defamatory, obscene, vulgar or otherwise offensive or in violation of our Prohibited Items Policy, Community Policy or any part of our Terms. You also agree not to post any content that is false and misleading or uses the Services in a manner that is fraudulent or deceptive.</p>
+
+        <h2>5. Your License To Use the Services</h2>
+
+        <p>Santakani gives you a personal, worldwide, royalty-free, non-assignable and non-exclusive license to use the software that is provided to you by Santakani as part of the Services. This license is for the sole purpose of enabling you to use and enjoy the benefit of the Services as provided by Santakani, in the manner permitted by these Terms and the following restrictions in particular:</p>
+
+        <ol>
+            <li>Don’t Use Our Services to Break the Law. You agree that you will not violate any laws in connection with your use of the Services. This includes any local, state, federal and international laws that may apply to you.</li>
+            <li>Pay Your Bills. You are responsible for paying all fees that you owe to Santakani. You are also solely responsible for collecting and/or paying any applicable taxes for any purchases or sales you make through our Services.</li>
+            <li>Don’t Try to Harm Our Systems. You agree not to interfere with or try to disrupt our Services, for example by distributing a virus or other harmful computer code.</li>
+            <li>Share Your Ideas. We would like to get suggestions and ideas from you! They can help us improve your experience and our Services.</li>
+        </ol>
+
+        <h2>6. Termination</h2>
+
+        <h3>6.1. Termination By You</h3>
+
+        <p>You may terminate your account with Santakani at any time from your account settings.Terminating your account will not affect the availability of some of Your Content that you posted through the Services prior to termination. And you’ll still have to pay any outstanding bills.</p>
+
+        <h3>6.2. Termination By Santakani</h3>
+
+        <p>We may terminate or suspend your account (and any related accounts) and your access to the Services at any time, for any reason, and without advance notice. If we do so, it’s important to understand that you don’t have a contractual or legal right to continue to use our Services, for example, to sell or buy on our website or mobile apps. Santakani may refuse service to anyone, at any time, for any reason.</p>
+
+        <p>If you or Santakani terminate your account, you may lose any information associated with your account, including Your Content.</p>
+
+        <h3>6.3. We May Discontinue the Services</h3>
+
+        <p>Santankani reserves the right to change, suspend or discontinue any of the Services at any time, for any reason. We will not be liable to you for the effect that any changes to the Services may have on you, including your income or your ability to generate revenue through the Services.</p>
+
+        <p>The Terms will remain in effect even after your access to the Service is terminated, or your use of the Service ends.</p>
+
+        <h2>7. Warranties and Limitation of Liability</h2>
+
+        <h3>7.1. Warranties</h3>
+
+        <p>We do not guarantee that:</p>
+
+        <ol>
+            <li>the Services will be secure or available at any particular time or location;</li>
+            <li>any defects or errors will be corrected;</li>
+            <li>the Services will be free of viruses or other harmful materials;</li>
+            <li>the results of using the Services will meet your expectations.</li>
+        </ol>
+
+        <p>You use the Services solely at your own risk. Some jurisdictions do not allow limitations on implied warranties, so the above limitations may not apply to you.</p>
+
+        <h3>7.2. Liability Limits</h3>
+
+        <p>To the fullest extent permitted by law, neither Santakani, nor our employees or directors shall be liable to you for any lost profits or revenues, or for any consequential, incidental, indirect, special or punitive damages arising out of or in connection with the Services or these Terms. In no event shall Santakani’s aggregate liability for any damages.</p>
+
+        <p>Some jurisdictions do not allow limitations on incidental or consequential damages, so the above limitations may not apply to you.</p>
+
+        <p>In addition , we do not response for:</p>
+
+        <dl>
+            <dt>Content You Access</dt>
+
+            <dd>You may come across materials that you find offensive or inappropriate while using our Services. We make no representations concerning any content posted by users through the Services. Santakani is not responsible for the accuracy, copyright compliance, legality or decency of content posted by users that you accessed through the Services. You release us from all liability relating to that content.</dd>
+
+            <dt>People You Interact With</dt>
+
+            <dd>You can use the Services to interact with other individuals, either online or in person. However, you understand that we do not screen users of our Services, and you release us from all liability relating to your interactions with other users.</dd>
+
+            <dt>Third-Party Services</dt>
+
+            <dd>Our Services may contain links to third-party websites or services that we don’t own or control (for example, links to Facebook, Twitter, and Pinterest). You may also need to use a third party’s product or service in order to use some of our Services (like a compatible mobile device to use our mobile apps). When you access these third-party services, you do so at your own risk. The third parties may require you to accept their own terms of use. Santakani is not a party to those agreements; they are solely between you and the third party.</dd>
+        </dl>
+
+        <h2>9. Indemnification</h2>
+
+        <p>We reserve the right to handle our legal defense however we see fit, even if you are indemnifying us, in which case you agree to cooperate with us so we can execute our strategy.</p>
+
+        <h2>10. Disputes</h2>
+
+        <h3>10.1. Disputes with Other Users</h3>
+
+        <p>If you find yourself in a dispute with another user of Santakani’s Services or a third party, we encourage you to contact the other party and try to resolve the dispute amicably.</p>
+
+        <p>You release Santakani from any claims, demands and damages arising out of disputes with other users or parties.</p>
+
+        <h3>10.2. Disputes with Santakani</h3>
+
+        <p>If you’re upset with us, let us know, and hopefully we can resolve your issue. But if we can’t, then the laws of Finland will govern any legal dispute involving our Services.</p>
+
+        <h2>11. Changes to the Terms</h2>
+
+        <p>We may update this policy from time to time. If we believe that the changes are material, we’ll definitely let you know by doing one (or more) of the following:</p>
+
+        <ol>
+            <li>posting the changes on the Site and Apps;</li>
+            <li>sending you an email or message about the changes;</li>
+            <li>posting an update in the version notes on the Apps’ platform.</li>
+        </ol>
+
+        <p>Changes will be effective upon the posting of the changes. You are responsible for reviewing and becoming familiar with any changes. Your use of the Site or App following the changes constitutes your acceptance of the updated policy.</p>
+
+        <h2>12. Others</h2>
+
+        <p>The Terms, including all of the policies that make up the Terms, supersede any other agreement between you and Santakani regarding the Services. If any part of the Terms is found to be unenforceable, that part will be limited to the minimum extent necessary so that the Terms will otherwise remain in full force and effect. Our failure to enforce any part of the Terms is not a waiver of our right to later enforce that or any other part of the Terms. We may assign any of our rights and obligations under the Terms.</p>
+
+        <hr>
+
+        <p>Effective: July 15, 2016</p>
+
+        <p>Thoughts or questions about thisTerms of service? Please <a href="mailto:contact@santakani.com">contact us</a>.</p>
+    </div>
+@endsection
