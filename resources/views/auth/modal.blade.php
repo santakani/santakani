703e6a52b5f9616df8f7cfaa49e73574e41401f0
Guo Yunhe <guoyunhebrave@gmail.com>
2016-07-16 14:51:15 +0300

    Auth layout and modal width

--- resources/views/auth/modal.blade.php
+++ resources/views/auth/modal.blade.php
@@ -1,5 +1,5 @@
 <div id="auth-modal" class="modal fade" tabindex="-1" role="dialog">
-    <div class="modal-dialog modal-sm">
+    <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
