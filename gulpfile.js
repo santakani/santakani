e77abac576efa83ad6497536ca192319f0ccac0c
Guo Yunhe <guoyunhebrave@gmail.com>
2016-07-16 20:32:14 +0300

    Remove requirejs dependency

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 var elixir = require('laravel-elixir');
 
-require('laravel-elixir-requirejs');
-
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
