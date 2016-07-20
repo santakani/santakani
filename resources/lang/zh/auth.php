9241e5289e4d6296de5369ba6f60b19edcaa435c
Guo Yunhe <guoyunhebrave@gmail.com>
2016-07-20 18:29:14 +0300

    Download translations

--- resources/lang/zh/auth.php
+++ resources/lang/zh/auth.php
@@ -22,7 +22,7 @@ return array(
     'request_password_reset_link' => '获取密码重置链接',
 
     // Auth system messages
-    'failed' => 'Email address or password is incorrect.',
+    'failed' => '电子邮箱或密码不正确',
     'throttle' => '登录过于频繁。请等待 :seconds 秒之后再尝试。',
 
 );
