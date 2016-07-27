4be44f08693039b9bd3f08c270185bcc15104972
Guo Yunhe <guoyunhebrave@gmail.com>
2016-07-27 18:30:05 +0300

    About page community and team section

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -67,41 +67,41 @@
     </section>
 
     <section id="community-section">
-        <div class="container">
-            <h2>{{ trans('common.community') }}</h2>
-            <p>Designers, artisits, craftsmen and craftswomen, design students, and design lovers consist of this unique community. People from different cultures, using different languages, meet each other for the same reason: passion of design.</p>
-            <p>Here, design lovers can share their thoughts and interact with designers. Without boundary of cultures and languages, the world of design is broader.</p>
-            <p>Here, designers spread their knowledge, techniques and ideas inside design with people. Design students can also learn a lot from stories of experienced designers.</p>
-            <p>Here, creativity and diversity are honored. Designers are praised by their design rather than business success.</p>
+        <div class="text container">
+            Designers, artisits, craftsmen and craftswomen, design students, and design lovers consist of this unique community. People from different cultures, using different languages, meet each other for the same reason: passion of design.
         </div>
+        <div class="background"></div>
     </section>
 
     <section id="team-section">
         <div class="container">
-            <h2>{{ trans('common.team') }}</h2>
+            <h2>Our Lovely Team</h2>
 
             <div class="row">
-                <div class="col-xs-6 col-sm-4">
+                <div class="col-sm-4">
                     <div class="team-member">
-                        <img class="img-rounded" src="/img/team/duyuexin.jpg">
+                        <img src="/img/team/duyuexin.jpg">
                         <h3>Du Yuexin</h3>
                         <p>co-founder, designer, business development</p>
+                        <p>Aalto University, School of Arts</p>
                         <a href="mailto:yuexin.du@santakani.com">yuexin.du@santakani.com</a>
                     </div>
                 </div>
-                <div class="col-xs-6 col-sm-4">
+                <div class="col-sm-4">
                     <div class="team-member">
-                        <img class="img-rounded" src="/img/team/guoyunhe.jpg">
+                        <img src="/img/team/guoyunhe.jpg">
                         <h3>Guo Yunhe</h3>
                         <p>co-founder, programmer, technical support</p>
+                        <p>Aalto University, School of Arts</p>
                         <a href="mailto:yunhe.guo@santakani.com">yunhe.guo@santakani.com</a>
                     </div>
                 </div>
-                <div class="col-xs-6 col-xs-offset-3 col-sm-4 col-sm-offset-0">
+                <div class="col-sm-4">
                     <div class="team-member">
-                        <img class="img-rounded" src="/img/team/yunxiaotong.jpg">
+                        <img src="/img/team/yunxiaotong.jpg">
                         <h3>Yun Xiaotong</h3>
                         <p>co-founder, editor, marketing, customer service</p>
+                        <p>Aalto University, School of Business</p>
                         <a href="mailto:xiaotong.yun@santakani.com">xiaotong.yun@santakani.com</a>
                     </div>
                 </div>
