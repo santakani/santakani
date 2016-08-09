f25ff310ee8b6fbb15d9d7e2166effe5d153f9c8
Guo Yunhe <guoyunhebrave@gmail.com>
2016-08-09 13:01:55 +0300

    Fix #43

--- resources/views/pages/user/show.blade.php
+++ resources/views/pages/user/show.blade.php
@@ -2,47 +2,72 @@
     'title' => $user->name . ' - ' . trans('common.user'),
     'body_id' => 'user-show-page',
     'body_classes' => ['user-show-page', 'user-page', 'show-page'],
+    'og_title' => $user->name,
+    'og_url' => $user->url,
+    'og_description' => $user->description,
+    'og_image' => $user->large_avatar_url,
 ])
 
 @section('header')
-
-@endsection
-
-@section('main')
-    <div class="container">
-        <br/><br/>
-        <div class="row">
-            <div class="col-sm-4 col-md-3">
-                <img class="img-rounded img-responsive" src="{{ $user->avatar(300) }}" width="300" height="300"/>
-                <h1>{{ $user->name }}</h1>
-                <p class="text-muted">{{ $user->description }}</p>
-                <p><i class="fa fa-clock-o"></i> {{ $user->created_at }}</p>
-            </div>
-            <div class="col-sm-8 col-md-9">
-                <div id="story-list" class="story-list row">
-                    @foreach ($stories as $story)
-                        <div class="col-sm-6 col-md-4">
-                            <article id="story-{{ $story->id }}" class="story material-card" data-id="{{ $story->id }}">
-                                <a href="{{ $story->url }}">
-                                    @if ($image = $story->image)
-                                        <img class="cover-image" src="{{ $image->fileUrl('thumb') }}" />
-                                    @else
-                                        <img class="cover-image" src="http://placehold.it/300x300?text=NO+IMAGE" />
-                                    @endif
-                                    <div class="shadow"></div>
-                                    <div class="text">
-                                        <h1>{{ $story->title }}</h1>
-                                        {{ $story->excerpt('content') }}
-                                    </div>
-                                </a>
-                            </article>
-                        </div>
-                    @endforeach
-                </div><!-- #story-list -->
-                <div class="text-center">
-                    {!! $stories->appends(app('request')->all())->links() !!}
-                </div>
+    <div class="article-list-header">
+        <div class="container">
+            <img class="avatar" src="{{ $user->medium_avatar_url }}"
+                srcset="{{ $user->large_avatar_url }} 2x" width="150" height="150"/>
+            <div class="text">
+                <h1 class="user-name">
+                    {{ $user->name }}
+                    @if (Auth::user()->id === $user->id)
+                        <a class="btn btn-default" href="/setting/profile">
+                            <i class="fa fa-pencil"></i>
+                            {{ trans('common.edit') }}
+                        </a>
+                    @endif
+                </h1>
+                <p class="description">
+                    @if ($user->description)
+                        {{ $user->description }}
+                    @else
+                        No description.
+                    @endif
+                    @if (Auth::user()->id === $user->id)
+                        <a href="/setting/profile">
+                            <i class="fa fa-pencil"></i>
+                            {{ trans('common.edit') }}
+                        </a>
+                    @endif
+                </p>
+                @if ($user->created_at)
+                    <p class="date">{{ $user->created_at->formatLocalized(App\Localization\Languages::dateFormat()) }}</p>
+                @endif
             </div>
         </div>
     </div>
+@endsection
+
+@section('main')
+    <section id="story-list" class="article-list">
+        <div class="list">
+            @foreach ($stories as $story)
+                <article>
+                    <a href="{{ $story->url }}">
+                        <div class="cover">
+                            @if ($story->image_id)
+                                <img class="image" src="{{ $story->image->thumb_file_url }}"
+                                    srcset="{{ $story->image->largethumb_file_url }} 2x"/>
+                            @endif
+                        </div>
+                        <div class="text">
+                            <div class="inner">
+                                <h3>{{ $story->title }}</h3>
+                                <p class="excerpt">{{ $story->excerpt('content') }}</p>
+                            </div>
+                        </div>
+                    </a>
+                </article>
+            @endforeach
+        </div>
+        <div class="pagination-wrap">
+            {!! $stories->appends(app('request')->all())->links() !!}
+        </div>
+    </section>
 @endsection
