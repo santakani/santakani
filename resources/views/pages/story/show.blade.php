825ebf7d2d6752e2922f4db304b9a21e6b2efbba
Guo Yunhe <guoyunhebrave@gmail.com>
2016-08-14 00:49:02 +0300

    Fix #51

--- resources/views/pages/story/show.blade.php
+++ resources/views/pages/story/show.blade.php
@@ -53,14 +53,5 @@
 @endsection
 
 @section('main')
-<div class="container">
-    <div class="row">
-        <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
-
-            <div class="content">
-                {!! $story->html('content') !!}
-            </div>
-        </div><!-- .col -->
-    </div><!-- .row -->
-</div><!-- .container -->
+    <div class="page-content">{!! $story->html('content') !!}</div>
 @endsection
