4bb2378599a31adf53e76dbef8d5499527f884eb
Guo Yunhe <guoyunhebrave@gmail.com>
2016-08-27 17:14:30 +0300

    DesignTranslation fillable #57

--- app/DesignTranslation.php
+++ app/DesignTranslation.php
@@ -10,4 +10,14 @@ class DesignTranslation extends Translation
      * @var string
      */
     protected $table = 'design_translation';
+
+    /**
+     * The attributes that are mass assignable. id, story_id, locale and timestamps
+     * are protected from vulnerability.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'design_id', 'locale', 'name', 'content',
+    ];
 }
