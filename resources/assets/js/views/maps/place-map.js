29fc8aab2502a7535f4cb1886d576550e518508f
Guo Yunhe <guoyunhebrave@gmail.com>
2016-10-22 18:09:41 +0300

    Replace OpenStreetMap with Mapbox tiles #87

--- resources/assets/js/views/maps/place-map.js
+++ resources/assets/js/views/maps/place-map.js
@@ -90,8 +90,8 @@ module.exports = Backbone.View.extend({
 
         this.map = Leaflet.map('place-map').setView([this.latitude, this.longitude], 13);
 
-        this.tile = Leaflet.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        this.tile = Leaflet.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2FudGFrYW5pIiwiYSI6ImNpcW02em1lZzAwMWpoeW5tdmRiOHh4MTcifQ.sE-MLInkW3KwjlwoaaKuAQ', {
+            attribution: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
             detectRetina: true,
         }).addTo(this.map);
 
