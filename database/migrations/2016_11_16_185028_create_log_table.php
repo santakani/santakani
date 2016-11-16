421978bd0b513c36a522e3b6d1c5b8785cc3641c
Guo Yunhe <guoyunhebrave@gmail.com>
2016-11-16 22:30:35 +0200

    Create database table for activity log #95

---
+++ database/migrations/2016_11_16_185028_create_log_table.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of Santakani
+ *
+ * (c) Guo Yunhe <guoyunhebrave@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Create log table for system activity log
+ *
+ * @see https://github.com/santakani/santakani/wiki/Activity-Log
+ */
+class CreateLogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('log', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('action')->nullable()->index();
+            $table->text('message')->nullable();
+
+            $table->string('target_type')->nullable();
+            $table->integer('target_id')->unsigned()->nullable();
+
+            $table->integer('user_id')->unsigned()->nullable();
+
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('user')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('log');
+    }
+}
