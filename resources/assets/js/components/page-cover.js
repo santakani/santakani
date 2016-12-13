8d14b9a28df23800e1d035eae9ed0a63d26bebd5
Guo Yunhe <guoyunhebrave@gmail.com>
2016-12-13 16:23:49 +0200

    Page cover support video background #97

--- resources/assets/js/components/page-cover.js
+++ resources/assets/js/components/page-cover.js
@@ -1,1 +1,28 @@
-$('.page-cover').outerHeight($(window).outerHeight() - $('.custom-navbar').outerHeight());
+resizePageCover();
+
+$(window).resize(resizePageCover);
+
+function resizePageCover() {
+    $('.page-cover').outerHeight($(window).outerHeight() - $('.custom-navbar').outerHeight());
+    var headerHeight = $(window).outerHeight() - $('.custom-navbar').outerHeight();
+    var headerWidth = $('.page-cover').outerWidth();
+    var videoOriginalWidth = $('.page-cover .video-background').attr('width');
+    var videoOriginalHeight = $('.page-cover .video-background').attr('height');
+
+    if (headerWidth / headerHeight > videoOriginalWidth / videoOriginalHeight) {
+        var videoWidth = headerWidth;
+        var videoHeight = videoOriginalHeight * headerWidth / videoOriginalWidth;
+
+    } else {
+        var videoWidth = videoOriginalWidth * headerHeight / videoOriginalHeight;
+        var videoHeight = headerHeight;
+    }
+
+    $('.page-cover .video-background').css({
+        width: videoWidth,
+        height: videoHeight,
+        left: (headerWidth - videoWidth) / 2,
+        top: (headerHeight - videoHeight) / 2,
+    });
+
+}
