7ba30edea98c09b21fb2612cef177513ae0f0a3d
Guo Yunhe <guoyunhebrave@gmail.com>
2016-12-24 15:57:59 +0200

    Remove image and taggable table seeder file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -29,17 +29,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserTableSeeder::class);
-        $this->call(ImageTableSeeder::class);
+
         $this->call(TagTableSeeder::class);
 
         $this->call(CountryTableSeeder::class);
         $this->call(CityTableSeeder::class);
+
         $this->call(DesignerTableSeeder::class);
         $this->call(PlaceTableSeeder::class);
         $this->call(StoryTableSeeder::class);
-
-        $this->call(TaggableTableSeeder::class);
-
         $this->call(DesignTableSeeder::class);
     }
 }
