560f4c27bcd3c110895ae20ca89db86252d3ec3e
Guo Yunhe <guoyunhebrave@gmail.com>
2017-01-18 22:59:04 +0200

    HTML tag misspell

--- resources/views/auth/modal.blade.php
+++ resources/views/auth/modal.blade.php
@@ -5,7 +5,7 @@
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             </div>
             <div class="modal-body">
-                <from class="form">
+                <form class="form">
                     <div class="form-group">
                         <a class="btn btn-primary btn-lg btn-block"
                             href="{{ app_redirect_url('auth/facebook') }}">
