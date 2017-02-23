f4e916a30c3281933ae1cb40ed0ca0cf8fa17aa8
Guo Yunhe <guoyunhebrave@gmail.com>
2017-02-23 15:59:21 +0200

    Designer: add links and tags to overview tab

--- resources/views/pages/designer/show-overview.blade.php
+++ resources/views/pages/designer/show-overview.blade.php
@@ -61,19 +61,83 @@ Overview tab
 
 <div id="overview-likes">
     <p class="text-muted">{{ trans_choice('common.like_count', $designer->likes()->count()) }}</p>
+    <p>
+        @foreach ($likes as $like)
+            <span class="like">
+                <a class="link-unstyled" href="{{ $like->user->url }}">
+                    <img src="{{ $like->user->avatar('small') }}" srcset="{{ $like->user->avatar('medium') }} x3" width="50" height="50">
+                    {{ $like->user->name }}
+                </a>
+            </span>
+        @endforeach
+    </p>
+</div>
 
-    @foreach ($likes as $like)
-        <span class="like">
-            <a class="link-unstyled" href="{{ $like->user->url }}">
-                <img src="{{ $like->user->avatar('small') }}" srcset="{{ $like->user->avatar('medium') }} x3" width="50" height="50">
-                {{ $like->user->name }}
+<ul id="social-links" class="social-links list-unstyled">
+    @if ($designer->facebook)
+        <li>
+            <a class="link-unstyled text-nowrap" href="{{ $designer->facebook }}" target="_blank">
+                <span class="icon ion-social-facebook"></span>
+                Facebook
             </a>
-        </span>
-    @endforeach
-</div>
+        </li>
+    @endif
+    @if ($designer->twitter)
+        <li>
+            <a class="link-unstyled text-nowrap" href="{{ $designer->twitter }}" target="_blank">
+                <span class="icon ion-social-twitter"></span>
+                Twitter
+            </a>
+        </li>
+    @endif
+    @if ($designer->instagram)
+        <li>
+            <a class="link-unstyled text-nowrap" href="{{ $designer->instagram }}" target="_blank">
+                <span class="icon ion-social-instagram"></span>
+                Instagram
+            </a>
+        </li>
+    @endif
+    @if ($designer->pinterest)
+        <li>
+            <a class="link-unstyled text-nowrap" href="{{ $designer->pinterest }}" target="_blank">
+                <span class="icon ion-social-pinterest"></span>
+                Pinterest
+            </a>
+        </li>
+    @endif
+    @if ($designer->vimeo)
+        <li>
+            <a class="link-unstyled text-nowrap" href="{{ $designer->vimeo }}" target="_blank">
+                <span class="icon ion-social-vimeo"></span>
+                Vimeo
+            </a>
+        </li>
+    @endif
+    @if ($designer->youtube)
+        <li>
+            <a class="link-unstyled text-nowrap" href="{{ $designer->youtube }}" target="_blank">
+                <span class="icon ion-social-youtube"></span>
+                YouTube
+            </a>
+        </li>
+    @endif
+    @if ($designer->website)
+        <li>
+            <a class="link-unstyled text-nowrap" href="{{ $designer->website }}" target="_blank">
+                <span class="icon ion-earth"></span>
+                Website
+            </a>
+        </li>
+    @endif
+    @if ($designer->email)
+        <li>
+            <a class="link-unstyled text-nowrap" href="mailto:{{ $designer->email }}">
+                <span class="icon ion-at"></span>
+                Email
+            </a>
+        </li>
+    @endif
+</ul>
 
-<div class="text-right">
-    <a class="btn btn-link" href="{{ $designer->url }}?tab=likes">
-        {{ trans('common.more') }}...
-    </a>
-</div>
+@include('components.tags.tags-hash', ['tags' => $designer->tags, 'linked' => true])
