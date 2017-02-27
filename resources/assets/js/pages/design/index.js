8bfca862876b546ecb61444a03f834d7c478734c
Guo Yunhe <guoyunhebrave@gmail.com>
2017-02-27 23:20:56 +0200

    Design list: apply new design card templates

--- resources/assets/js/pages/design/index.js
+++
@@ -1,5 +0,0 @@
-$('.tag-filter a').click(function (e) {
-    e.preventDefault();
-    $('.tag-filter input').val($(this).parents('li').data('id'));
-    $(this).parents('form').submit();
-});
--- resources/views/pages/design/index.blade.php
+++ resources/views/pages/design/index.blade.php
@@ -6,49 +6,19 @@
 ])
 
 @section('main')
-    <section id="design-filter" class="article-filter">
-        <form action="/design" method="get">
-            <div class="form-group">
-                <label>{{ trans('common.search') }}</label>
-                <input type="search" name="search" value="{{ request()->input('search') }}"
-                       class="form-control" placeholder="{{ trans('common.search') }}"
-                       maxlength="50"/>
-            </div>
-            <div class="form-group">
-                <label>{{ trans('common.tag') }}</label>
-                @include('components.tag-filter', ['selected' => request()->input('tag_id')])
-            </div>
-        </form>
-    </section>
-
-    <section id="design-list" class="article-list">
-        <div class="list">
+<div id="design-list">
+    <div class="container">
+        <div class="row">
             @foreach ($designs as $design)
-                <article>
-                    <a href="{{ $design->url }}">
-                        <div class="cover">
-                            @if ($design->image_id)
-                                <img class="image" src="{{ $design->image->thumb_file_url }}"
-                                    srcset="{{ $design->image->largethumb_file_url }} 2x"/>
-                            @else
-                                <img class="image" src="{{ url('img/placeholder/square.png') }}"/>
-                            @endif
-                            @if ($design->logo_id)
-                                <img class="logo" src="{{ $design->logo->small_file_url }}"/>
-                            @endif
-                        </div>
-                        <div class="text">
-                            <div class="inner">
-                                <h2>{{ $design->text('name') }}<br></h2>
-                                <div class="excerpt">{{ $design->excerpt('content') }}</div>
-                            </div>
-                        </div>
-                    </a>
-                </article>
+                <div class="col-sm-6 col-md-4">
+                    @include('components.cards.design-card', ['design' => $design])
+                </div>
             @endforeach
         </div>
-        <div class="pagination-wrap">
+
+        <div class="text-center">
             {!! $designs->appends(app('request')->all())->links() !!}
         </div>
-    </section><!-- #design-list -->
+    </div><!-- /.container -->
+</div><!-- /#design-list -->
 @endsection
